981c4d354143b4ffa0866ca80f6c657ba099e243
Der-Alex <alexander.thimm@googlemail.com>
2018-12-10 16:13:37 +0100

    #87 BackendLoginService - added tests - extended testsuite

--- test.run.bat
+++ test.run.bat
@@ -1,1 +1,1 @@
-.\vendor\bin\phpunit --configuration test.phpunit.xml --log-junit test.junit.xml Tests
+.\vendor\bin\phpunit --configuration test.phpunit.xml --log-junit test.junit.xml --color=always Tests
