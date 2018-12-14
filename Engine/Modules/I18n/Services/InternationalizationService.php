6ae7e8971129edd8ce8b9687e782f49674e8b85d
mys3lf <matthaeus.gdaniec@gmx.de>
2018-12-14 09:32:50 +0100

    Added Generic CRUD module Start Internationalization Added a few additional exceptions other awesome stuff

--- Engine/Modules/I18n/Services/InternationalizationService.php
+++ Engine/Modules/I18n/Services/InternationalizationService.php
@@ -7,19 +7,39 @@
  */
 
 namespace Oforge\Engine\Modules\I18n\Services;
-use Oforge\Engine\Modules\I18n\Models\Snippets;
+
+use Monolog\Logger;
+use Oforge\Engine\Modules\I18n\Models\Snippet;
 
 
 /**
  * Class InternationalizationService
  * @package Oforge\Engine\Modules\I18n\Services
  */
-class InternationalizationService {
+class InternationalizationService
+{
     /**
      * InternationalizationService constructor.
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->em = Oforge()->DB()->getManager();
-        $this->repo = $this->em->getRepository(Snippets::class);
+        $this->repo = $this->em->getRepository(Snippet::class);
+    }
+
+    public function get($key, $language)
+    {
+        /**
+         * @var $element Snippet
+         */
+        $element = $this->repo->findOneBy(["scope" => $language, "name" => $key]);
+        if(isset($element)) return $element->getValue();
+
+        $element = Snippet::create(Snippet::class, ["scope" => $language, "name" => $key, "value" => $key]);
+
+        $this->em->persist($element);
+        $this->em->flush();
+
+        return $element->getValue();
     }
 }
