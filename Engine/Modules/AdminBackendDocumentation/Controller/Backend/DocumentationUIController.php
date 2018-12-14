7397d91d188f5aa21d049d96adf622b8a4f6af92
Steffen O <so@7pkonzepte.de>
2018-12-14 16:15:06 +0100

    Moved sidebar definition to bootstraps install method

--- Engine/Modules/AdminBackendDocumentation/Controller/Backend/DocumentationUIController.php
+++ Engine/Modules/AdminBackendDocumentation/Controller/Backend/DocumentationUIController.php
@@ -18,76 +18,10 @@ class DocumentationUIController extends SecureBackendController
 
     public function indexAction(Request $request, Response $response)
     {
-        $data = ['page_header' => 'Hello from the TestPlugin', 'page_header_description' => "Mega awesome optional additional description"];
-        Oforge()->View()->assign($data);
     }
 
     public function generalAction(Request $request, Response $response)
     {
-        /**
-         * @var $sidebarNavigation SidebarNavigationService
-         */
-        $sidebarNavigation = Oforge()->Services()->get("backend.sidebar.navigation");
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation",
-            "order" => 100
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_ui_elements",
-            "order" => 100,
-            "parent" => "backend_documentation",
-            "icon" => "fa fa-laptop"
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation_ui_general",
-            "order" => 1,
-            "parent" => "backend_ui_elements",
-            "icon" => "fa fa-circle-o",
-            "path" => "backend_documentation_ui_general"
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation_ui_icons",
-            "order" => 2,
-            "parent" => "backend_ui_elements",
-            "icon" => "fa fa-circle-o",
-            "path" => "backend_documentation_ui_icons"
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation_ui_buttons",
-            "order" => 3,
-            "parent" => "backend_ui_elements",
-            "icon" => "fa fa-circle-o",
-            "path" => "backend_documentation_ui_buttons"
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation_ui_silders",
-            "order" => 4,
-            "parent" => "backend_ui_elements",
-            "icon" => "fa fa-circle-o",
-            "path" => "backend_documentation_ui_silders"
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation_ui_timeline",
-            "order" => 5,
-            "parent" => "backend_ui_elements",
-            "icon" => "fa fa-circle-o",
-            "path" => "backend_documentation_ui_timeline"
-        ]);
-
-        $sidebarNavigation->put([
-            "name" => "backend_documentation_ui_modals",
-            "order" => 6,
-            "parent" => "backend_ui_elements",
-            "icon" => "fa fa-circle-o",
-            "path" => "backend_documentation_ui_modals"
-        ]);
 
     }
 
