8a20b1dded3b3f027611936422f2a7d30d4a313d
Der-Alex <alexander.thimm@googlemail.com>
2019-01-15 13:38:55 +0100

    - added cms models

---
+++ Engine/Modules/CMS/Models/Layout/Slot.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexander Wegner
+ * Date: 15.01.2019
+ * Time: 12:02
+ */
+
+namespace Oforge\Engine\Modules\CMS\Models\Layout;
+
+use Oforge\Engine\Modules\Core\Abstracts\AbstractModel;
+
+/**
+ * @ORM\Table(name="oforge_cms_slot")
+ * @ORM\Entity
+ */
+class Slot extends AbstractModel {
+    /**
+     * @var int
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="slot_name", type="string", nullable=false, unique=true)
+     */
+    private $name;
+    
+    /**
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+    
+    /**
+     * @param int $id
+     */
+    public function setId( int $id ): void {
+        $this->id = $id;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+    
+    /**
+     * @param string $name
+     */
+    public function setName( string $name ): void {
+        $this->name = $name;
+    }
+}
