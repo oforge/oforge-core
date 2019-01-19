21733576d355ebd5b4f9e944b1d84167fae0fbe2
FK <fk@7pkonzepte.de>
2019-01-19 22:30:58 +0100

    Helper class - Add data types - Add docs - Add BinaryHelper class - Add FileSystemHelper class

---
+++ Engine/Modules/Core/Helper/BinaryHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Oforge\Engine\Modules\Core\Helper;
+
+/**
+ * BinaryHelper
+ *
+ * @package Oforge\Engine\Modules\Core\Helper
+ */
+class BinaryHelper {
+    /**
+     * Prevent instance.
+     */
+    private function __construct() {
+    }
+
+    /**
+     * Add flag.
+     *
+     * @param int $flags
+     * @param int $flag
+     *
+     * @return bool
+     */
+    public static function add(int $flags, int $flag) : bool {
+        return $flags | $flag;
+    }
+
+    /**
+     * Check, if flag is set.
+     *
+     * @param int $flags
+     * @param int $flag
+     *
+     * @return bool
+     */
+    public static function is(int $flags, int $flag) : bool {
+        return (bool) ($flags & $flag);
+    }
+
+}
