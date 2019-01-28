03f85a8c6e68f1dff5d9db3a798af3db09237ab6
Der-Alex <alexander.thimm@googlemail.com>
2019-01-28 11:08:04 +0100

    scss variables

--- Themes/Base/Template.php
+++ Themes/Base/Template.php
@@ -2,5 +2,27 @@
 namespace Themes\Base;
 
 use Oforge\Engine\Modules\TemplateEngine\Abstracts\AbstractTemplate;
+use Oforge\Engine\Modules\TemplateEngine\Models\ScssVariable;
+use Oforge\Engine\Modules\TemplateEngine\Services\ScssVariableService;
 
-class Template extends AbstractTemplate {}
+class Template extends AbstractTemplate {
+    private $vars = [
+        [
+            'name' => '$primary',
+            'value' => '#000',
+            'type' => 'color',
+        ],
+        [
+            'name' => '$secondary',
+            'value' => '#fff',
+            'type' => 'color',
+        ],
+    ];
+
+    public function registerTemplateVariables() {
+
+        // $scssVariables->put('$primary', "#000", "1", "color", "0", "Base");
+
+    }
+}
+
