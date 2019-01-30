ef5ba8e04a9e7cb8c8a9f3c99e545081a2fbba05
steffen <steffenosterburg@googlemail.com>
2019-01-30 11:41:31 +0100

    Reformatted SystemSettings

--- Engine/Modules/SystemSettings/Controller/Backend/SystemSettingsController.php
+++ Engine/Modules/SystemSettings/Controller/Backend/SystemSettingsController.php
@@ -2,17 +2,20 @@
 
 namespace Oforge\Engine\Modules\SystemSettings\Controller\Backend;
 
-
 use Oforge\Engine\Modules\AdminBackend\Abstracts\SecureBackendController;
 use Oforge\Engine\Modules\Auth\Models\User\BackendUser;
 use Oforge\Engine\Modules\Core\Services\ConfigService;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class SystemSettingsController extends SecureBackendController
-{
-    public function indexAction(Request $request, Response $response)
-    {
+class SystemSettingsController extends SecureBackendController {
+    /**
+     * @param Request $request
+     * @param Response $response
+     *
+     * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
+     */
+    public function indexAction(Request $request, Response $response) {
         /**
          * @var $configService ConfigService
          */
@@ -20,12 +23,10 @@ class SystemSettingsController extends SecureBackendController
 
         $data = $configService->groups();
 
-        Oforge()-> View()->assign(["config" => $data]);
+        Oforge()->View()->assign(["config" => $data]);
     }
 
-
-    public function initPermissions()
-    {
+    public function initPermissions() {
         $this->ensurePermissions("indexAction", BackendUser::class, BackendUser::ROLE_ADMINISTRATOR);
     }
 
