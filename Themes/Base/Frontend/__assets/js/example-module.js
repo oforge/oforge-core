032e3b6da1a295abe1c5f4acd89c83cd503f0c2d
Der Alex <alexander.thimm@googlemail.com>
2019-02-05 17:16:23 +0100

    added example module to frontend part of base theme

---
+++ Themes/Base/Frontend/__assets/js/example-module.js
@@ -0,0 +1,24 @@
+// check if the Oforge namespace exists
+if (typeof Oforge !== 'undefined') {
+    // if it exists, it should have the register function, so register your module
+    // the properties "name", "selector" and "init" are required
+    // name: the name of your module
+    // selector: the html selector to search for. If it is found, the module can be initialized
+    // init: the function to initialize the module. This function gets called automatically from the module-loader.js
+    // when the DOMContentLoaded event is triggered.
+    Oforge.register({
+        name: 'exampleModule',
+        selector: '.wrap',
+        otherNotRequiredContent: 'some other content, that we can define and that is not required',
+        init: function () {
+            var self = this;
+            var htmlElement = document.querySelectorAll(self.selector);
+            console.log("the module " + self.name + ' was registered and initialized successfully!', htmlElement);
+            // ...
+            // do more stuff
+            // ...
+        }
+    });
+} else {
+    console.warn("Oforge is not defined. Module cannot be registered.");
+}
