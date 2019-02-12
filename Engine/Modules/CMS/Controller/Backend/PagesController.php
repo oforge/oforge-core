c3a5f731857dc1277cd1223da85e179eaebd855e
Laxity Media <info@laxitymedia.com>
2019-02-12 23:28:40 +0000

    - added content loader functions to page builder service
    - revised abstract content type class and content type implementations

--- Engine/Modules/CMS/Controller/Backend/PagesController.php
+++ Engine/Modules/CMS/Controller/Backend/PagesController.php
@@ -11,7 +11,7 @@ namespace Oforge\Engine\Modules\CMS\Controller\Backend;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractController;
-use Oforge\Engine\Modules\CMS\Services\PageService;
+use Oforge\Engine\Modules\CMS\Services\PageTreeService;
 use Oforge\Engine\Modules\CMS\Services\PageBuilderService;
 
 class PagesController extends AbstractController {
@@ -23,12 +23,14 @@ class PagesController extends AbstractController {
      * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
      */
     public function indexAction(Request $request, Response $response) {
-        $contentTypeService = OForge()->Services()->get("content.type");
-        $pageBuilderService = OForge()->Services()->get("pages.tree.view");
+        $contentTypeService = OForge()->Services()->get("content.type.service");
+        $pageTreeService = OForge()->Services()->get("page.tree.service");
+        $pageBuilderService = OForge()->Services()->get("page.builder.service");
         
         $data = [
-            "js" => ["cms_page_controller_jstree_config" => $pageBuilderService->generateJsTreeConfigJSON()],
-            "pages" => $pageBuilderService->getPageArray(),
+            "js" => ["cms_page_controller_jstree_config" => $pageTreeService->generateJsTreeConfigJSON()],
+            "pages" => $pageTreeService->getPageArray(),
+            "content" => $pageBuilderService->getPageArray(2),
             "contentTypeGroups" => $contentTypeService->getContentTypeGroupArray(),
             "post" => $_POST
             
--- Engine/Modules/CMS/Services/PageBuilderService.php
+++ Engine/Modules/CMS/Services/PageBuilderService.php
@@ -2,7 +2,21 @@
 
 namespace Oforge\Engine\Modules\CMS\Services;
 
+use Doctrine\ORM\PersistentCollection;
+use Oforge\Engine\Modules\I18n\Models\Language;
+use Oforge\Engine\Modules\I18n\Models\Snippet;
+use Oforge\Engine\Modules\CMS\Models\Layout\Layout;
+use Oforge\Engine\Modules\CMS\Models\Layout\Slot;
+use Oforge\Engine\Modules\CMS\Models\Site\Site;
 use Oforge\Engine\Modules\CMS\Models\Page\Page;
+use Oforge\Engine\Modules\CMS\Models\Page\PagePath;
+use Oforge\Engine\Modules\CMS\Models\Page\PageContent;
+use Oforge\Engine\Modules\CMS\Models\Content\ContentTypeGroup;
+use Oforge\Engine\Modules\CMS\Models\Content\ContentType;
+use Oforge\Engine\Modules\CMS\Models\Content\Content;
+use Oforge\Engine\Modules\CMS\ContentTypes\Row;
+use Oforge\Engine\Modules\CMS\ContentTypes\RichText;
+use Oforge\Engine\Modules\CMS\ContentTypes\Image;
 
 class PageBuilderService
 {
@@ -12,21 +26,43 @@ class PageBuilderService
     public function __construct()
     {
         $this->entityManager = Oforge()->DB()->getManager();
-        $this->repository = $this->entityManager->getRepository(Page::class);
+        $this->pageContentRepository = $this->entityManager->getRepository(PageContent::class);
+        $this->pageRepository = $this->entityManager->getRepository(Page::class);
     }
     
     /**
-     * Returns all available page entities
-     * 
-     * @return Page[]|NULL
+     * Return page entity for given page id
+     * @param int $pathId
+     *
+     * @return Page|NULL
      */
-    public function getPageEntities()
+    private function getPageContentEntities(int $pathId)
     {
-        $pageEntityArray = $this->repository->findAll();
+        $pageContentEntities = $this->pageContentRepository->findBy(["pagePath" => $pathId]);
         
-        if (isset($pageEntityArray))
+        if (isset($pageContentEntities))
         {
-            return $pageEntityArray;
+            return $pageContentEntities;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+    
+        /**
+     * Return page entity for given page id
+     * @param int $id
+     * 
+     * @return Page|NULL
+     */
+    private function getPageEntity(int $id)
+    {
+        $pageEntity = $this->pageRepository->findOneBy(["id" => $id]);
+        
+        if (isset($pageEntity))
+        {
+            return $pageEntity;
         }
         else
         {
@@ -35,77 +71,176 @@ class PageBuilderService
     }
     
     /**
-     * Returns all found pages as an associative array
-     * 
-     * @return array|NULL Array filled with available pages
+     * Returns the content type group found as an associative array
+     * @param ContentTypeGroup $contentTypeGroup
+     *
+     * @return array|NULL Array filled with available content type group data
      */
-    public function getPageArray()
+    private function getContentTypeGroupArray(?ContentTypeGroup $contentTypeGroupEntity)
     {
-        $pageEntities = $this->getPageEntities();
-        
-        if (!$pageEntities)
-        {
-            return NULL;    
-        }
-        
-        $pages = [];
-        foreach($pageEntities as $pageEntity)
-        {
-            $page = [];
-            $page["id"] = $pageEntity->getId();
-            $page["name"] = $pageEntity->getName();
-            $page["parent"] = $pageEntity->getParent();
-            
-            $pathEntities = $pageEntity->getPaths();
-            
-            $pathArray = [];
-            foreach($pathEntities as $pathEntity)
-            {
-                $pathArray[] = $pathEntity->getPath();
-            }
-            
-            $page["paths"] = $pathArray;
-            
-            $pages[] = $page;
-        }
-        
-        return $pages;
-    }
-    
-    /**
-     * Generate a jsTree configuration file with page data included
-     * 
-     * @return array|NULL jsTree configuration file as PHP array
-     */
-    public function generateJsTreeConfigJSON()
-    {
-        $pages = $this->getPageArray();
-        
-        if (!$pages)
+        if (!$contentTypeGroupEntity)
         {
             return NULL;
         }
         
-        $jsTreePageArray = [];
-        foreach ($pages as $page)
+        $contentTypeGroup = [];
+        $contentTypeGroup["id"] = $contentTypeGroupEntity->getId();
+        $contentTypeGroup["name"] = $contentTypeGroupEntity->getName();
+        $contentTypeGroup["description"] = $contentTypeGroupEntity->getDescription();
+        
+        return $contentTypeGroup;
+    }
+    
+    /**
+     * Returns the content type found as an associative array
+     * @param ContentType $contentTypeEntity
+     *
+     * @return array|NULL Array filled with available content type data
+     */
+    private function getContentTypeArray(?ContentType $contentTypeEntity)
+    {
+        if (!$contentTypeEntity)
         {
-            $jsTreePageArray[] = [
-                "id" => $page["id"],
-                "parent" => $page["parent"] ? $page["parent"] : "#",
-                "text" => $page["name"]
-            ];
+            return NULL;
         }
         
-        $jsTreeJSON = [
-            "core" => [
-                "animation" => 0,
-                "check_callback" => TRUE,
-                "force_text" => TRUE,
-                "themes" => ["stripes" => FALSE],
-                "data" => $jsTreePageArray
-            ]
-        ];
+        $contentType = [];
+        $contentType["id"] = $contentTypeEntity->getId();
+        $contentType["group"] = $this->getContentTypeGroupArray($contentTypeEntity->getGroup());
+        $contentType["name"] = $contentTypeEntity->getName();
+        $contentType["icon"] = $contentTypeEntity->getIcon();
+        $contentType["description"] = $contentTypeEntity->getDescription();
+        $contentType["classPath"] = $contentTypeEntity->getClassPath();
         
-        return $jsTreeJSON;
+        return $contentType;
+    }
+    
+    /**
+     * Returns the content found as an associative array
+     * @param Content $contentEntity
+     *
+     * @return array|NULL Array filled with available content data
+     */
+    private function getContentArray(?Content $contentEntity)
+    {
+        if (!$contentEntity)
+        {
+            return NULL;
+        }
+        
+        $content = [];
+        $content["id"] = $contentEntity->getId();
+        $content["type"] = $this->getContentTypeArray($contentEntity->getType());
+        $content["parent"] = $contentEntity->getParent();
+        $content["name"] = $contentEntity->getName();
+        $content["cssClass"] = $contentEntity->getCssClass();
+        $content["data"] = $contentEntity->getData();
+        
+        return $content;
+    }
+    
+    /**
+     * Returns all found page contents as an associative array
+     * @param int $pathId
+     *
+     * @return array|NULL Array filled with available page contents
+     */
+    private function getPageContentArray(int $pathId)
+    {
+        $pageContentEntities = $this->getPageContentEntities($pathId);
+        
+        if (!$pageContentEntities)
+        {
+            return NULL;
+        }
+        
+        $pageContents = [];
+        foreach($pageContentEntities as $pageContentEntity)
+        {
+            $pageContent = [];
+            $pageContent["id"] = $pageContentEntity->getId();
+            $pageContent["content"] = $this->getContentArray($pageContentEntity->getContent());
+            $pageContent["order"] = $pageContentEntity->getOrder();
+            
+            $pageContents[] = $pageContent;
+        }
+        
+        return $pageContents;
+    }
+    
+    /**
+     * Returns the language found as an associative array
+     * @param Language $languageEntity
+     *
+     * @return array|NULL Array filled with available language data
+     */
+    private function getLanguageArray(?Language $languageEntity)
+    {
+        if (!$languageEntity)
+        {
+            return NULL;
+        }
+        
+        $language = [];
+        $language["id"] = $languageEntity->getId();
+        $language["iso"] = $languageEntity->getIso();
+        $language["name"] = $languageEntity->getName();
+        $language["active"] = $languageEntity->isActive();
+        
+        return $language;
+    }
+    
+     /**
+     * Returns all found page paths as an associative array
+     * @param PersistentCollection $pathEntities
+     *
+     * @return array|NULL Array filled with available paths
+     */
+    private function getPathArray(?PersistentCollection $pathEntities)
+    {
+        if (!$pathEntities)
+        {
+            return NULL;
+        }
+        
+        $paths = [];
+        foreach($pathEntities as $pathEntity)
+        {
+            $path = [];
+            $path["id"] = $pathEntity->getId();
+            $path["language"] = $this->getLanguageArray($pathEntity->getLanguage());
+            $path["path"] = $pathEntity->getPath();
+            $path["pageContent"] = $this->getPageContentArray($path["id"]);
+            
+            $paths[] = $path;
+        }
+        
+        return $paths;
+    }
+    
+    /**
+     * Returns the page found as an associative array
+     * @param int $id
+     *
+     * @return array|NULL Array filled with available page data
+     */
+    public function getPageArray(int $id)
+    {
+        $pageEntity = $this->getPageEntity($id);
+        
+        if (!$pageEntity)
+        {
+            return NULL;
+        }
+        
+        $page = [];
+        $page["id"] = $pageEntity->getId();
+        $page["layout"] = $pageEntity->getLayout();
+        $page["site"] = $pageEntity->getSite();
+        $page["name"] = $pageEntity->getName();
+        $page["parent"] = $pageEntity->getParent();
+        $page["paths"] = $this->getPathArray($pageEntity->getPaths());
+        
+        return $page;
     }
 }
