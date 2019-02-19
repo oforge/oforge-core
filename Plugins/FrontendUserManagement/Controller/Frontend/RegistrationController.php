6c9eb8e340787563bfb55e6496e28d5146da50a2
Der-Alex <alexander.thimm@googlemail.com>
2019-02-19 15:25:23 +0100

    Frontend login and registration

--- Plugins/FrontendUserManagement/Controller/Frontend/RegistrationController.php
+++ Plugins/FrontendUserManagement/Controller/Frontend/RegistrationController.php
@@ -3,6 +3,8 @@
 namespace FrontendUserManagement\Controller\Frontend;
 
 use FrontendUserManagement\Services\RegistrationService;
+use Oforge\Engine\Modules\Auth\Services\AuthService;
+use Oforge\Engine\Modules\Auth\Services\PasswordService;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractController;
 use Oforge\Engine\Modules\Session\Services\SessionManagementService;
 use Slim\Http\Request;
@@ -14,26 +16,43 @@ class RegistrationController extends AbstractController {
     }
 
     /**
+     * Register the user and send the user to the profile page
+     *
      * @param Request $request
      * @param Response $response
      *
      * @return Response
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
      */
     public function processAction(Request $request, Response $response) {
+        /** @var AuthService $authService */
+        /** @var PasswordService $passwordService */
+        /** @var RegistrationService $registrationService */
+        /** @var SessionManagementService $sessionManagementService*/
+        /** @var Router $router */
+        $authService                = null;
+        $passwordService            = null;
+        $registrationService        = null;
+        $sessionManagementService   = null;
+        $router                     = null;
+        $uri                        = null;
+        $body                       = null;
+        $jwt                        = null;
+        $email                      = null;
+        $password                   = null;
+        $passwordConfirm            = null;
+        $user                       = null;
+
         if (empty($_SESSION)) {
             print_r("No session :/");
             die();
         }
 
-        /** @var RegistrationService $registrationService */
-        $registrationService = Oforge()->Services()->get('frontend.user.management.registration');
-
-        /**
-         * @var $router Router
-         */
-        $router = Oforge()->App()->getContainer()->get("router");
-        $uri = $router->pathFor("frontend_registration");
+        $registrationService    = Oforge()->Services()->get('frontend.user.management.registration');
+        $router                 = Oforge()->App()->getContainer()->get("router");
+        $uri                    = $router->pathFor("frontend_registration");
 
         /**
          * disallow direct processAction call. Only post action is allowed
@@ -42,8 +61,10 @@ class RegistrationController extends AbstractController {
             return $response->withRedirect($uri, 302);
         }
 
-        $body = $request->getParsedBody();
-        $jwt = null;
+        $body               = $request->getParsedBody();
+        $email              = $body['frontend_registration_email'];
+        $password           = $body['frontend_registration_password'];
+        $passwordConfirm    = $body['frontend_registration_password_confirm'];
 
         /**
          * no token was sent
@@ -65,24 +86,32 @@ class RegistrationController extends AbstractController {
         /**
          * no email or password body was sent
          */
-        if (!array_key_exists("frontend_registration_email", $body) ||
-            !array_key_exists("frontend_registration_password", $body) ||
-            !array_key_exists("frontend_registration_password_confirm", $body)) {
+        if (!$email || !$password || !$passwordConfirm) {
             return $response->withRedirect($uri, 302);
         }
 
         /**
          * Password and password confirmation are not equal
          */
-        if ($body['frontend_registration_password'] !== $body['frontend_registration_password_confirm']) {
+        if ($password !== $passwordConfirm) {
             return $response->withRedirect($uri, 302);
         }
 
+        $passwordService    = Oforge()->Services()->get('password');
+        $authService        = Oforge()->Services()->get('auth');
+        $password           = $passwordService->hash($password);
+        $user               = $registrationService->register($email, $password);
+
         /**
-         * TODO: - implement registration
-         *       - registration creates new jwt to store in the session.
+         * Registration failed
+         * Maybe someone tried to register with an email address that is already in use
+         * TODO: respond to the registration process with a nice information?
          */
-        $jwt = $registrationService->register();
+        if (!$user) {
+            return $response->withRedirect($uri, 302);
+        }
+
+        $jwt = $authService->createJWT($user);
 
         /**
          * $jwt is null if the login credentials are incorrect
@@ -91,15 +120,16 @@ class RegistrationController extends AbstractController {
             return $response->withRedirect($uri, 302);
         }
 
-        /**
-         * @var $sessionManagement SessionManagementService
-         */
-        $sessionManagement = Oforge()->Services()->get('session.management');
-        $sessionManagement->regenerateSession();
+        $sessionManagementService = Oforge()->Services()->get('session.management');
+        $sessionManagementService->regenerateSession();
 
         $_SESSION['auth'] = $jwt;
 
         $uri = $router->pathFor("frontend_profile");
+
+        /**
+         * TODO: Send Email with registration information
+         */
 
         return $response->withRedirect($uri, 302);
     }
--- Plugins/FrontendUserManagement/Services/RegistrationService.php
+++ Plugins/FrontendUserManagement/Services/RegistrationService.php
@@ -2,12 +2,43 @@
 
 namespace FrontendUserManagement\Services;
 
+use FrontendUserManagement\Models\User;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
 
-class RegistrationService {
-    public function register() {
-        // TODO: implement registration
-        return false;
+class RegistrationService extends AbstractDatabaseAccess {
+
+    public function __construct() {
+        parent::__construct(['default' => User::class]);
+    }
+
+    /**
+     * @param string $email
+     * @param string $password
+     *
+     * @return array|null
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function register(string $email, string $password) {
+        $user = null;
+
+        if (!$this->userExists($email)) {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setPassword($password);
+            $this->entityManager()->persist($user);
+            $this->entityManager()->flush();
+
+            $user = $user->toArray();
+            unset($user["password"]);
+            $user["type"] = User::class;
+        }
+
+        return $user;
+    }
+
+    private function userExists(string $email) {
+        return $this->repository()->findBy(['email' => $email]);
     }
 
 }
