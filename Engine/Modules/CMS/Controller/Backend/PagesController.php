3a581d50ad4a61705b38c034d0a436df27c20e20
Laxity Media <info@laxitymedia.com>
2019-03-11 22:33:00 +0000

    - moved logic from pages controller to pages controller service
    - added custom menu to page builder page jstree
    - added custom callback functions for page jstree custom menu

--- Engine/Modules/CMS/Controller/Backend/PagesController.php
+++ Engine/Modules/CMS/Controller/Backend/PagesController.php
@@ -11,8 +11,6 @@ namespace Oforge\Engine\Modules\CMS\Controller\Backend;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractController;
-use Oforge\Engine\Modules\CMS\Services\PageTreeService;
-use Oforge\Engine\Modules\CMS\Services\PageBuilderService;
 
 class PagesController extends AbstractController {
     /**
@@ -23,71 +21,16 @@ class PagesController extends AbstractController {
      * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
      */
     public function indexAction(Request $request, Response $response) {
-        $contentTypeService = OForge()->Services()->get("content.type.service");
-        $pageTreeService    = OForge()->Services()->get("page.tree.service");
-        $pageBuilderService = OForge()->Services()->get("page.builder.service");
+        $pagesControllerService = OForge()->Services()->get("pages.controller.service");
         
-        $selectedPage       = isset($_POST["cms_page_jstree_selected_page"]) && $_POST["cms_page_jstree_selected_page"] > 0 ? $_POST["cms_page_jstree_selected_page"] : 0;
-        $selectedLanguage   = isset($_POST["cms_page_selected_language"])    && $_POST["cms_page_selected_language"] > 0    ? $_POST["cms_page_selected_language"]    : 0;
-        $selectedElement    = isset($_POST["cms_page_selected_element"])     && !empty($_POST["cms_page_selected_element"]) ? $_POST["cms_page_selected_element"]     : 0;
-        $selectedAction     = isset($_POST["cms_page_selected_action"])      && !empty($_POST["cms_page_selected_action"])  ? $_POST["cms_page_selected_action"]      : 'edit';
-        
-        $data = [
-            "js"                => ["cms_page_controller_jstree_config" => $pageTreeService->generateJsTreeConfigJSON()],
-            "pages"             => $pageTreeService->getPageArray(),
-            "contentTypeGroups" => $contentTypeService->getContentTypeGroupArray(),
-            "selectedElement"   => $selectedElement,
-            "post"              => $_POST
-        ];
-
-        if ($selectedPage)
+        switch ($_POST["cms_form"])
         {
-            $pageArray        = $pageBuilderService->getPageArray($selectedPage);
-            $pageContents     = $pageArray["paths"][$selectedLanguage]["pageContent"];
-            
-            if ($selectedElement)
-            {
-                $data["contents"] = $pageBuilderService->getContentDataArrayById($pageContents, $selectedElement);
-                
-                $selectedElementIdArray = explode("-", $selectedElement);
-                $selectedElementId = end($selectedElementIdArray);
-                
-                $data["__selectedElement"] = $selectedElement; // TODO: just used as development info
-                $data["__selectedElementId"] = $selectedElementId; // TODO: just used as development info
-                $data["__selectedElementTypeId"] = $data["contents"]["typeId"]; // TODO: just used as development info
-                $data["__selectedAction"] = $selectedAction; // TODO: just used as development info
-                
-                if (is_numeric($selectedElementId) && $selectedElementId > 0)
-                {
-                    $selectedElementTypeId = $data["contents"]["typeId"];
-                    
-                    if (!is_numeric($selectedElementTypeId))
-                    {
-                        $selectedElementTypeId = 0;
-                    }
-                    
-                    switch ($selectedAction)
-                    {
-                        case "submit":
-                            // persist new content element data to database and reload content data from database
-                            $data["contentElementData"] = $contentTypeService->setContentDataArray($selectedElementId, $selectedElementTypeId, $_POST)->getContentDataArray($selectedElementId, $selectedElementTypeId);
-                            $data["contents"]           = $pageBuilderService->getContentDataArrayById($pageContents, $selectedElement);
-                            break;
-                        case "delete":
-                            break;
-                        default:
-                            // action equals 'edit' or is unknown
-                            $data["contentElementData"] = $contentTypeService->getContentDataArray($selectedElementId, $selectedElementTypeId);
-                            break;
-                    }
-                }
-            }
-            else
-            {
-                $data["contents"] = $pageBuilderService->getContentDataArray($pageContents);
-            }
-            
-            $data["pageBuilderData"] = $pageArray; // TODO: just used as development info
+            case "cms_page_jstree_form":
+                break;
+            case "cms_page_builder_form":
+            default:
+                $data = $pagesControllerService->getSelectedPageData($_POST);
+                break;
         }
         
         Oforge()->View()->assign($data);
--- Themes/Base/Backend/__assets/js/cms.page.controller.js
+++ Themes/Base/Backend/__assets/js/cms.page.controller.js
@@ -1,10 +1,59 @@
-// jsTree callback function
-$('#cms_page_controller_jstree').on('changed.jstree', function (e, data) {
+// jsTree callback functions
+$('#cms_page_controller_jstree').on('loaded.jstree', function (event, data) {
+	$('#cms_page_controller_jstree').jstree('open_all');
+});
+
+$('#cms_page_controller_jstree').on('select_node.jstree', function (event, data) {
 	// switch page on page select
 	$('#cms_page_jstree_selected_page').val($('#cms_page_controller_jstree').jstree('get_selected'));
 	$('#cms_page_selected_element').val('');
 	$('#cms_page_builder_form').submit();
 });
+
+$('#cms_page_controller_jstree').on('create_node.jstree', function (event, data) {
+	var node = data.node;
+	var parent = data.parent;
+	var position = data.position;
+	
+	console.log("#!!! A new node has been created:");
+	console.log("Page Id: " + node.id);
+	console.log("Page Parent: " + node.parent);
+	console.log("Page Name: " + node.text);
+	console.log("-");
+	console.log("New node was added to parent: " + parent);
+	console.log("New node was added at position: " + position);
+	console.log("---");
+});
+
+$('#cms_page_controller_jstree').on('rename_node.jstree', function (event, data) {
+	var node = data.node;
+	var text = data.text;
+	var old = data.old;
+	
+	console.log("#!!! A node has been renamed:");
+	console.log("Page Id: " + node.id);
+	console.log("Page Parent: " + node.parent);
+	console.log("Page Name: " + node.text);
+	console.log("-");
+	console.log("Node was renamed to: " + text);
+	console.log("Node was renamed from: " + old);
+	console.log("---");
+});
+
+$('#cms_page_controller_jstree').on('delete_node.jstree', function (event, data) {
+	var node = data.node;
+	var parent = data.parent;
+	
+	console.log("#!!! A node has been deleted:");
+	console.log("Page Id: " + node.id);
+	console.log("Page Parent: " + node.parent);
+	console.log("Page Name: " + node.text);
+	console.log("-");
+	console.log("Node was deleted from parent: " + parent);
+	console.log("---");
+});
+
+
 
 // mark and select selectable elements in page builder
 $('[data-pb-id]').each(
@@ -35,6 +84,23 @@ $('[data-pb-id]').each(
 				}
 			);
 		}
+	}
+);
+
+//on click create new root page
+$('#cms-page-builder-create-new-root-page').click(
+	function() {
+	    var tree = $('#cms_page_controller_jstree').jstree(true);
+	    var node = tree.get_node("#");
+    	
+	    node = tree.create_node(node, {"type":"folder"});
+	    tree.edit(node);
+	    
+    	console.log("Creating page:");
+    	console.log("Page Id: " + node.id);
+    	console.log("Page Parent: " + node.parent);
+    	console.log("Page Name: " + node.text);
+    	console.log("---");
 	}
 );
 
@@ -80,7 +146,70 @@ $(window).resize(function() {
 
 // bind functions to document load event
 $(document).ready(function() {
-    $('#cms_page_controller_jstree').jstree(cms_page_controller_jstree_config);
+	// create jstree configs
+	var jsTreeCoreConfig   = cms_page_controller_jstree_config;
+	var jsTreeCustomConfig = {
+		"plugins" : ["contextmenu"],
+		"contextmenu" : {
+		    "select_node" : false,
+		    "show_at_node" : true,
+		    "items" : {
+		        "createItem": {
+		            "label": "Create",
+		            "action": function (obj) {
+		        	    var tree = $('#cms_page_controller_jstree').jstree(true);
+		        	    var node = tree.get_node(obj.reference);
+		            	
+		        	    node = tree.create_node(node, {"type":"folder"});
+		        	    tree.edit(node);
+		        	    
+		            	console.log("Creating page:");
+		            	console.log("Page Id: " + node.id);
+		            	console.log("Page Parent: " + node.parent);
+		            	console.log("Page Name: " + node.text);
+		            	console.log("---");
+		            }
+		        },
+		        "renameItem": {
+		            "label": "Rename",
+		            "action": function (obj) {
+		        	    var tree = $('#cms_page_controller_jstree').jstree(true);
+		        	    var node = tree.get_node(obj.reference);
+		        	    
+		        	    tree.edit(node);
+		        	    
+		            	console.log("Renaming page:");
+		            	console.log("Page Id: " + node.id);
+		            	console.log("Page Parent: " + node.parent);
+		            	console.log("Page Name: " + node.text);
+		            	console.log("---");
+		            }
+		        },
+		        "deleteItem": {
+		            "label": "Delete",
+		            "action": function (obj) {
+		        	    var tree = $('#cms_page_controller_jstree').jstree(true);
+		        	    var node = tree.get_node(obj.reference);
+		        	    
+		        	    tree.delete_node(node);
+		        	    
+		            	console.log("Deleting page:");
+		            	console.log("Page Id: " + node.id);
+		            	console.log("Page Parent: " + node.parent);
+		            	console.log("Page Name: " + node.text);
+		            	console.log("---");
+		            }
+		        }
+		    }
+		}
+	};
+	
+	// merge jstree configs
+	var jsTreeConfig = Object.assign(jsTreeCoreConfig, jsTreeCustomConfig);
+	
+	// create jstree object
+    $('#cms_page_controller_jstree').jstree(jsTreeConfig);
+    
     $('#cms_page_builder_language_selector').change(
     	function() {
     		$('#cms_page_selected_language').val($('#cms_page_builder_language_selector option:selected').val());
