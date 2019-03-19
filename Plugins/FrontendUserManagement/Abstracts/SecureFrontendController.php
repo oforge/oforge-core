230f74fce5e9e32537fb648a4d0f27f4d506f2c0
Der-Alex <alexander.thimm@googlemail.com>
2019-03-19 16:47:58 +0100

    updated middleware management

--- Plugins/FrontendUserManagement/Abstracts/SecureFrontendController.php
+++ Plugins/FrontendUserManagement/Abstracts/SecureFrontendController.php
@@ -5,7 +5,4 @@ namespace FrontendUserManagement\Abstracts;
 use Oforge\Engine\Modules\Auth\Controller\SecureController;
 
 class SecureFrontendController extends SecureController {
-    protected function ensurePermissions($method, $userType, $role = null) {
-        parent::ensurePermissions($method, $userType, $role = null);
-    }
 }
