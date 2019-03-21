f52431b554119ca281743cb0a7ca38b140f8100a
Steffen <steffenosterburg@googlemail.com>
2019-03-21 21:29:45 +0100

    Helpdesk admin frontend and core

--- Plugins/Helpdesk/Controller/Backend/BackendHelpdeskMessengerController.php
+++ Plugins/Helpdesk/Controller/Backend/BackendHelpdeskMessengerController.php
@@ -2,17 +2,57 @@
 
 namespace Helpdesk\Controller\Backend;
 
+use Helpdesk\Services\HelpdeskMessengerService;
 use Oforge\Engine\Modules\AdminBackend\Core\Abstracts\SecureBackendController;
 use Oforge\Engine\Modules\Auth\Models\User\BackendUser;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class BackendHelpdeskMessengerController extends SecureBackendController{
-    public function indexAction(Request $reqeust, Response $response) {
+    /**
+     * @param Request $reqeust
+     * @param Response $response
+     * @param $args
+     *
+     * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
+     */
+    public function indexAction(Request $reqeust, Response $response, $args) {
+        $ticketId = $args['id'];
+        /** @var HelpdeskMessengerService $helpdeskMessengerService */
+        $helpdeskMessengerService = Oforge()->Services()->get('helpdesk.messenger');
 
+        $messages = $helpdeskMessengerService->getConversationByTarget($ticketId);
+
+        Oforge()->View()->assign(['messages' => $messages]);
+    }
+
+    /**
+     * @param Request $reqeust
+     * @param Response $response
+     * @param $args
+     *
+     * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
+     * @throws \Exception
+     */
+    public function sendAction(Request $request, Response $response, $args) {
+        if($request->isPost()) {
+            $targetId = $args['id'];
+
+            $message = $request->getParsedBody()['message'];
+
+            /** @var HelpdeskMessengerService $helpdeskMessengerService */
+            $helpdeskMessengerService = Oforge()->Services()->get('helpdesk.messenger');
+
+            $conversation = $helpdeskMessengerService->getConversationByTarget($targetId);
+
+            $helpdeskMessengerService->sendMessage($conversation->getId(), 'helpdesk?', $message);
+
+            $response->withRedirect('/backend/helpdesk/messenger/' . $args['id']);
+        }
+        $response->withRedirect('/');
     }
 
     public function initPermissions() {
-        $this->ensurePermissions("indexAction", BackendUser::class, BackendUser::ROLE_MODERATOR);
+        $this->ensurePermissions('indexAction', BackendUser::class, BackendUser::ROLE_MODERATOR);
     }
 }
--- Plugins/Helpdesk/Models/Ticket.php
+++ Plugins/Helpdesk/Models/Ticket.php
@@ -35,7 +35,7 @@ class Ticket extends AbstractModel {
      * @var string
      * @ORM\Column(name="ticket_issue_type", type="string", nullable=false)
      */
-    private $issue;
+    private $issueType;
 
     /**
      * @var string
@@ -55,4 +55,103 @@ class Ticket extends AbstractModel {
      * @ORM\Column(name="timestamp", type="datetime", nullable=false);
      */
     private $timestamp;
+
+    /**
+     * @return int
+     */
+    public function getId() : int {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOpener() : string {
+        return $this->opener;
+    }
+
+    /**
+     * @param string $opener
+     *
+     * @return Ticket
+     */
+    public function setOpener(string $opener) : Ticket {
+        $this->opener = $opener;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus() : string {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return Ticket
+     */
+    public function setStatus(string $status) : Ticket {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIssueType() : string {
+        return $this->issueType;
+    }
+
+    /**
+     * @param string $issue
+     *
+     * @return Ticket
+     */
+    public function setIssueType(string $issueType) : Ticket {
+        $this->issue = $issueType;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle() : string {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return Ticket
+     */
+    public function setTitle(string $title) : Ticket {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage() : string {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return Ticket
+     */
+    public function setMessage(string $message) : Ticket {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getTimestamp() : \DateTime {
+        return $this->timestamp;
+    }
 }
