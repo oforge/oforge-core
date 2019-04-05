f56f79a6f49c5a72021571c972f343293fbb12f7
Basti <sk@7pkonzepte.de>
2019-04-05 14:26:48 +0200

    Added Templates

--- Plugins/TestMail/Controller/Frontend/TestMailController.php
+++ Plugins/TestMail/Controller/Frontend/TestMailController.php
@@ -9,24 +9,26 @@ use Slim\Http\Response;
 class TestMailController extends AbstractController {
     /**
      * Returns rendered Mail to the browser for quick access/testing
+     * To display mail, go to: /testmail?template=<template-name>
      * @param Request $request
      * @param Response $response
      * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
      */
     public function indexAction(Request $request, Response $response) {
+
         $testOptions = [
-            "to"         => ['test@test.de', 'test2@test.com'],
+            "to"         => [ ],
             "cc"         => [ ],
             "bcc"        => [ ],
             "replyTo"    => [ ],
             "subject"    => "",
             "html"      => true,
             "attachment" => [ ],
-            "template" => "Test.twig",
+            "template" => $request->getQueryParam('template')
         ];
 
         $mailservice = Oforge()->Services()->get('mail');
-        $data = ['mail'=> $testOptions, 'data'=> ['activationLink' => 'blablub']];
+        $data = ['mail'=> $testOptions, 'data'=> []];
         $mail = $mailservice->renderMail($data);
         echo $mail;
         exit();
