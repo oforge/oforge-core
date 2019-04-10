0e15f0e08602e025ac3be7a2bedad431d85b3858
FK <fk@7pkonzepte.de>
2019-04-10 18:10:59 +0200

    replace old addFlashMessage with new flash()->addMessage

--- Plugins/FrontendUserManagement/Controller/Frontend/LogoutController.php
+++ Plugins/FrontendUserManagement/Controller/Frontend/LogoutController.php
@@ -2,12 +2,19 @@
 
 namespace FrontendUserManagement\Controller\Frontend;
 
+use Oforge\Engine\Modules\I18n\Helper\I18N;
 use Oforge\Engine\Modules\Session\Services\SessionManagementService;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Router;
 
+/**
+ * Class LogoutController
+ *
+ * @package FrontendUserManagement\Controller\Frontend
+ */
 class LogoutController {
+
     /**
      * @param Request $request
      * @param Response $response
@@ -16,17 +23,15 @@ class LogoutController {
      * @throws \Exception
      */
     public function indexAction(Request $request, Response $response) {
-        /**
-         * @var $sessionManager SessionManagementService
-         */
+        /** @var SessionManagementService $sessionManager */
         $sessionManager = Oforge()->Services()->get('session.management');
         $sessionManager->sessionDestroy();
         $sessionManager->sessionStart();
-        /**
-         * @var $router Router
-         */
+        /** @var Router $router */
         $router = Oforge()->App()->getContainer()->get('router');
-        Oforge()->View()->addFlashMessage('success', 'You have been logged out.');
+        Oforge()->View()->Flash()->addMessage('success', I18N::translate('logout_success', 'You have been logged out.'));
+
         return $response->withRedirect($router->pathFor('frontend_login'), 302);
     }
+
 }
