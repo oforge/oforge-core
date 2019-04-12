87d96b4730ff08de075dab2a6e76ed4ae1ea9cb2
FK <fk@7pkonzepte.de>
2019-04-12 17:05:36 +0200

    index.php: if timelimit<90 set to 90

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
  */
 define('ROOT_PATH', __DIR__);
 
+if (((int) ini_get('max_execution_time')) < 90) {
+    set_time_limit(90);
+}
+
 require_once ROOT_PATH . '/vendor/autoload.php';
 
 $smith = BlackSmith::getInstance();
