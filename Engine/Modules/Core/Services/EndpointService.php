f5aeacc5f80bd6a0989e32573b3a46e06b7f7c94
FK <fk@7pkonzepte.de>
2019-04-17 20:59:36 +0200

    remove old function

--- Engine/Modules/Core/Services/EndpointService.php
+++ Engine/Modules/Core/Services/EndpointService.php
@@ -12,7 +12,6 @@ use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointAction as EndpointAction;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointClass;
 use Oforge\Engine\Modules\Core\Exceptions\ConfigOptionKeyNotExists;
-use Oforge\Engine\Modules\Core\Helper\ArrayHelper;
 use Oforge\Engine\Modules\Core\Helper\Statics;
 use Oforge\Engine\Modules\Core\Helper\StringHelper;
 use Oforge\Engine\Modules\Core\Models\Endpoint\Endpoint as EndpointModel;
@@ -134,76 +133,6 @@ class EndpointService extends AbstractDatabaseAccess {
             }
         }
         $this->repository()->clear();
-    }
-
-    /**
-     * @param array $endpoints
-     *
-     * @return array
-     * @throws ConfigOptionKeyNotExists
-     */
-    protected function prepareEndpointConfigs1(array $endpoints) : array {
-        $endpointConfigs = [];
-
-        foreach ($endpoints as $path => $config) {
-            $isRoot = ($path === '/' || '');
-
-            if ($this->isValid($config)) {
-                $controller = $config['controller'];
-                $scope      = ArrayHelper::get($config, 'asset_scope', 'frontend');
-                $order      = ArrayHelper::get($config, 'order', 1337);
-                $methods    = ArrayHelper::get($config, 'methods', []);
-
-                $classMethods = get_class_methods($config['controller']);
-
-                if ($classMethods === null) {
-                    Oforge()->Logger()->get()->addWarning("Maybe some namespace, class or method was defined wrong.");
-                    // Oforge()->Logger()->get()->addWarning(json_encode($config));
-                    $classMethods = [];
-                }
-
-                if (!empty($methods)) {
-                    $classMethods = array_intersect(array_keys($methods), $classMethods);
-                }
-                foreach ($classMethods as $classMethod) {
-                    if (substr($classMethod, -6) !== 'Action') {
-                        continue;
-                    }
-                    if ($classMethod === 'indexAction' || isset($methods[$classMethod])) {
-                        $name       = ArrayHelper::get($config, 'name', str_replace('/', '_', $path));
-                        $actionPath = $path;
-                    } else {
-                        $action = substr($classMethod, 0, -6);
-                        if (isset($config['name'])) {
-                            $name = $config['name'] . '_' . $action;
-                        } else {
-                            $name = $path . ($isRoot ? '' : '/') . $action;
-                        }
-                        $actionPath = $path . ($isRoot ? '' : '/') . $action;
-                    }
-                    $httpMethod = ArrayHelper::get($methods, $classMethod, 'any');
-                    // if (!in_array($httpMethod, self::SLIM_ROUTE_METHODS)) {
-                    //     $httpMethod = 'any';
-                    // }
-
-                    $endpointConfigs[] = [
-                        'name'             => $name,
-                        'path'             => $actionPath,
-                        'controllerClass'  => $controller,
-                        'controllerMethod' => $classMethod,
-                        'asset_scope'      => $scope,
-                        'http_method'      => $httpMethod,
-                        'order'            => $order,
-                    ];
-                }
-                if (empty($endpointConfigs)) {
-                    Oforge()->Logger()->get()
-                            ->addWarning('An endpoint was defined but the corresponding controller has no method that ends with ...Action', $config);
-                }
-            }
-        }
-
-        return $endpointConfigs;
     }
 
     /**
