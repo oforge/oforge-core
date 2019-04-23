4c9ee1cf060a52861798490a173348f07b4759a1
FK <fk@7pkonzepte.de>
2019-04-23 13:04:50 +0200

    - remove timeout in console - replace magic timeout number with defined constant

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 /**
  * This is the oforge portal starting point.
  */
+define('OFORGE_SCRIPT_TIMEOUT', 90);
 define('ROOT_PATH', __DIR__);
 
-if (((int) ini_get('max_execution_time')) < 90) {
-    set_time_limit(90);
+if (((int) ini_get('max_execution_time')) < OFORGE_SCRIPT_TIMEOUT) {
+    set_time_limit(OFORGE_SCRIPT_TIMEOUT);
 }
 
 require_once ROOT_PATH . '/vendor/autoload.php';
