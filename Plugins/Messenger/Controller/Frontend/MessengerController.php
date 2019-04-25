f58436838582a3267169dfe374f74a5e6dc9950d
Der-Alex <alexander.thimm@googlemail.com>
2019-04-25 13:18:31 +0200

    UserService

--- Plugins/Messenger/Controller/Frontend/MessengerController.php
+++ Plugins/Messenger/Controller/Frontend/MessengerController.php
@@ -2,12 +2,15 @@
 
 namespace Messenger\Controller\Frontend;
 
+use Doctrine\ORM\ORMException;
 use FrontendUserManagement\Abstracts\SecureFrontendController;
 use FrontendUserManagement\Models\User;
+use Messenger\Models\Conversation;
 use Messenger\Services\FrontendMessengerService;
 use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Router;
 
 class MessengerController extends SecureFrontendController {
     /**
@@ -17,9 +20,16 @@ class MessengerController extends SecureFrontendController {
      *
      * @return Response
      * @throws ServiceNotFoundException
-     * @throws \Doctrine\ORM\ORMException
+     * @throws ORMException
      */
     public function indexAction(Request $request, Response $response, $args) {
+        /** @var FrontendMessengerService $frontendMessengerService */
+        $frontendMessengerService = Oforge()->Services()->get('frontend.messenger');
+        $user = Oforge()->View()->get('user');
+
+        /** @var Conversation[] $conversationList */
+        $conversationList = $frontendMessengerService->getConversationList($user['id']);
+        Oforge()->View()->assign(['conversationList' => $conversationList]);
 
         /* Get a conversation: /messages/conversationId */
         if (isset($args) && isset($args['id'])) {
@@ -28,10 +38,15 @@ class MessengerController extends SecureFrontendController {
                 return $response;
             }
 
-            /** @var FrontendMessengerService $frontendMessengerService */
-            $frontendMessengerService = Oforge()->Services()->get('frontend.messenger');
             $conversation = $frontendMessengerService->getConversationById($args['id']);
             Oforge()->View()->assign(['conversation' => $conversation]);
+        } else {
+            if (sizeof($conversationList) > 0) {
+                /** @var Router $router */
+                $router = Oforge()->App()->getContainer()->get('router');
+                $uri = $router->pathFor('frontend_account_messages') . '/' . $conversationList[0]->getId();
+                return $response->withRedirect($uri, 302);
+            }
         }
     }
 
