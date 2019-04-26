90a3f00dbf78fc1a0dbcbe81f1adadc5c434ecab
Der-Alex <alexander.thimm@googlemail.com>
2019-04-26 15:45:07 +0200

    added frontend message sending

--- Plugins/Messenger/Controller/Frontend/MessengerController.php
+++ Plugins/Messenger/Controller/Frontend/MessengerController.php
@@ -5,6 +5,7 @@ namespace Messenger\Controller\Frontend;
 use Doctrine\ORM\ORMException;
 use FrontendUserManagement\Abstracts\SecureFrontendController;
 use FrontendUserManagement\Models\User;
+use Helpdesk\Services\HelpdeskMessengerService;
 use Messenger\Models\Conversation;
 use Messenger\Services\FrontendMessengerService;
 use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
@@ -31,11 +32,29 @@ class MessengerController extends SecureFrontendController {
         $conversationList = $frontendMessengerService->getConversationList($user['id']);
         Oforge()->View()->assign(['conversationList' => $conversationList]);
 
+        /** @var Router $router */
+        $router = Oforge()->App()->getContainer()->get('router');
+
         /* Get a conversation: /messages/conversationId */
         if (isset($args) && isset($args['id'])) {
             /* Create a new message for a given conversation */
+
             if ($request->isPost()) {
-                return $response;
+                $conversationId = $args['id'];
+
+                $senderId = $request->getParsedBody()['sender'];
+                $message  = $request->getParsedBody()['message'];
+
+                /** @var FrontendMessengerService $frontendMessengerService */
+                $frontendMessengerService = Oforge()->Services()->get('frontend.messenger');
+
+                $conversation = $frontendMessengerService->getConversation($conversationId, $senderId);
+
+                $frontendMessengerService->sendMessage($conversation['id'], 'frontend', $senderId, $message);
+
+                $uri = $router->pathFor('frontend_account_messages');
+
+                return $response->withRedirect($uri . '/' . $conversation['id'], 302);
             }
 
             $conversation = $frontendMessengerService->getConversation($args['id'], $user['id']);
