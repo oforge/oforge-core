3df64e06ae87f5f8394ea74179de21dd00bdcfad
kevinkoessl <kk@7pkonzepte.de>
2019-04-29 14:30:54 +0200

    Mailchimp Plugin

--- Plugins/Mailchimp/Controller/Frontend/NewsletterSubscriptionController.php
+++ Plugins/Mailchimp/Controller/Frontend/NewsletterSubscriptionController.php
@@ -4,7 +4,6 @@ namespace Mailchimp\Controller\Frontend;
 
 use Mailchimp\Services\MailchimpNewsletterService;
 use \Oforge\Engine\Modules\Core\Abstracts\AbstractController;
-use Oforge\Engine\Modules\Mailer\Services\MailService;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -17,15 +16,43 @@ class NewsletterSubscriptionController extends AbstractController
 {
     public function indexAction(Request $request, Response $response)
     {
-        // show the email form for requesting a reset link
+        // show the email form for requesting a subscribing to the newsletter
     }
 
-    public function subscribeAction(Request $request, Response $response) {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     * @throws \Interop\Container\Exception\ContainerException
+     * @throws \Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException
+     */
+    public function subscribeAction(Request $request, Response $response)
+    {
         /** @var MailchimpNewsletterService $mailchimpNewsletterService */
         $mailchimpNewsletterService = Oforge()->Services()->get('mailchimp.newsletter');
+
         $router = Oforge()->Container()->get('router');
         $body = $request->getParsedBody();
-        $email = $body['email'];
+        $email = $body['frontend_newsletter_email'];
+        $uri = $router->pathFor('frontend_newsletter_subscription');
+
+        if (!$email) {
+            Oforge()->View()->addFlashMessage('warning', 'Invalid form data.');
+            return $response->withRedirect($uri, 302);
+        } else {
+            $mailchimpNewsletterService->addListMember("$email");
+            return $response->withRedirect($uri, 302);
+        }
+    }
+
+    public function unsubscribeAction(Request $request, Response $response)
+    {
+        /** @var MailchimpNewsletterService $mailchimpNewsletterService */
+        $mailchimpNewsletterService = Oforge()->Services()->get('mailchimp.newsletter');
+
+        $router = Oforge()->Container()->get('router');
+        $body = $request->getParsedBody();
+        $email = $body['frontend_newsletter_email'];
         $uri = $router->pathFor('frontend_newsletter_subscription');
 
         if (!$email) {
@@ -33,7 +60,8 @@ class NewsletterSubscriptionController extends AbstractController
 
             return $response->withRedirect($uri, 302);
         } else {
-            $mailchimpNewsletterService->addListMember($email);
+            $mailchimpNewsletterService->removeListMember("$email");
+            return $response->withRedirect($uri, 302);
         }
     }
 }
--- Plugins/Mailchimp/Services/MailchimpNewsletterService.php
+++ Plugins/Mailchimp/Services/MailchimpNewsletterService.php
@@ -4,51 +4,106 @@ namespace Mailchimp\Services;
 
 use FrontendUserManagement\Models\User;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
+use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
 
 class MailchimpNewsletterService extends AbstractDatabaseAccess
 {
+    protected $mailchimpUri;
 
+    protected $apiraven;
+
+    protected $configService;
+
+    protected $userName;
+
+    protected $apiKey;
+
+    protected $dataCenter;
+
+    protected $listId;
+
+    /**
+     * MailchimpNewsletterService constructor.
+     * @throws ServiceNotFoundException
+     */
     public function __construct()
     {
         parent::__construct(['default' => User::class]);
+
+        $this->configService = $configService = Oforge()->Services()->get("config");
+        $this->apiraven = Oforge()->Services()->get('apiraven');
     }
 
     /**
-     *
      * @param string $email_address
+     * @throws ServiceNotFoundException
      */
     public function addListMember(string $email_address)
     {
-//        $listId = "foo";
-//        $mailchimpUri = 'https://usX.api.mailchimp.com/3.0/lists/' . $listId . '/members';
-//
-//
-//        $data = [
-//            'email_address' => $email_address,
-//            'status' => 'subscribed',
-//        ];
-//
-//        $options = [
-//            'http' => [
-//                'header' => 'content-type: application/json',
-//                'method' => 'POST',
-//                'content' => http_build_query($data),
-//            ],
-//        ];
-//
-//        $context = stream_context_create($options);
-//        $result = file_get_contents($mailchimpUri, false, $context);
-//        if ($result === false) { /* Handle error */
-//        }
-//
-//        var_dump($result);
+        $this->updateParams();
+        $mailchimpUri = $this->mailchimpUri . '/lists/' . $this->listId;
+
+        $this->apiraven->setApi($mailchimpUri, $this->userName, $this->apiKey);
+
+        $data = [
+            'email_address' => $email_address,
+            'status' => 'pending',
+        ];
+
+        $result = $this->apiraven->post('members', $data);
+
+        if ($result != false) {
+            Oforge()->View()->addFlashMessage('success', 'You are subscribed successfully!');
+        } else {
+            Oforge()->View()->addFlashMessage('warning', 'Something went wrong...');
+        }
     }
 
-    public function removeListMember()
+    /**
+     * @param string $email_address
+     */
+    public function removeListMember(string $email_address)
     {
+        $this->updateParams();
+        $emailMd5 = md5(strtolower($email_address));
+
+        $mailchimpUri = $this->mailchimpUri . '/lists/' . $this->listId . '/members';
+
+        $this->apiraven->setApi($mailchimpUri, $this->userName, $this->apiKey);
+
+        $data = [
+            'email_address' => $email_address,
+            'status' => 'unsubscribed'
+        ];
+
+        $result = $this->apiraven->put($emailMd5, $data);
+        if ($result != '200') {
+            Oforge()->View()->addFlashMessage('success', 'If your email was known to us you are now unsubscribed successfully!');
+        } else if ($result == false) {
+            Oforge()->View()->addFlashMessage('warning', 'Something went wrong...');
+        }
     }
 
-    public function deleteListMember()
+    protected function updateParams()
     {
+        $this->userName = $this->configService->get("mailchimp_username");
+        $this->apiKey = $this->configService->get("mailchimp_api_key");
+        $this->dataCenter = $this->configService->get("mailchimp_data_center");
+        $this->listId = $this->configService->get("mailchimp_list_id");
+        $this->mailchimpUri = $this->configService->get("mailchimp_uri");
+        $this->mailchimpUri = $this->constructMailchimpUri($this->apiKey, $this->dataCenter);
+    }
+
+    /**
+     * @param string $api_key
+     * @param string $dataCenter
+     * @return string
+     */
+    protected function constructMailchimpUri(string $api_key, string $dataCenter)
+    {
+        $mailchimpUri = str_replace("{dc}", $dataCenter, $this->mailchimpUri);
+        return $mailchimpUri;
     }
 }
+
+
