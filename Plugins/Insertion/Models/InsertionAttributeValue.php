af3d9abc4f353962ebb2fdde433983044b1f5a41
steffen <steffenosterburg@googlemail.com>
2019-05-03 14:04:01 +0200

    Insertion association mapping

--- Plugins/Insertion/Models/InsertionAttributeValue.php
+++ Plugins/Insertion/Models/InsertionAttributeValue.php
@@ -9,7 +9,7 @@ use Oforge\Engine\Modules\Core\Abstracts\AbstractModel;
  * @ORM\Table(name="oforge_insertion_attribute_value")
  * @ORM\Entity
  */
-class Insertion_Attribute_Value extends AbstractModel {
+class InsertionAttributeValue extends AbstractModel {
     /**
      * @var int
      * @ORM\Column(name="id", type="integer", nullable=false)
@@ -19,26 +19,25 @@ class Insertion_Attribute_Value extends AbstractModel {
     private $id;
 
     /**
-     * TODO: Relation mapping
      * @var int
      * @ORM\Column(name="attribute_key_id", type="integer", nullable=false)
+     * @ORM\ManyToOne(targetEntity="AttributeKey")
+     * @ORM\JoinColumn(nullable=false)
      */
     private $attributeKeyId;
 
 
     /**
-     * TODO: Relation mapping
      * @var int
      * @ORM\Column(name="insertion_id", type="integer", nullable=false)
+     * @ORM\ManyToOne(targetEntity="Insertion")
+     * @ORM\JoinColumn(nullable=false)
      */
-    private $insertionTypeKeyId;
+    private $insertionId;
 
     /**
-     * TODO: Check if nullable is okay
      * @var string
      * @ORM\Column(name="insertion_attribute_value", type="string", nullable=true)
      */
     private $value;
-
-
 }
---
+++ Plugins/Insertion/Models/InsertionTypeAttribute.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Insertion\Models;
+
+use Doctrine\ORM\Mapping as ORM;
+use Oforge\Engine\Modules\Core\Abstracts\AbstractModel;
+
+/**
+ * @ORM\Table(name="oforge_insertion_type_attribute")
+ * @ORM\Entity
+ */
+class InsertionTypeAttribute extends AbstractModel {
+    /**
+     * @var int
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     * @ORM\Column(name="attribute_key_id", type="integer", nullable=false)
+     * @ORM\ManyToOne(targetEntity="AttributeKey")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $attributeKeyId;
+
+    /**
+     * @var int
+     * @ORM\Column(name="insertion_type_id", type="integer", nullable=false)
+     * @ORM\ManyToOne(targetEntity="InsertionType")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $insertionTypeId;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="required", type="boolean", nullable=false)
+     */
+    private $required;
+}
