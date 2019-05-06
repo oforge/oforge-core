e37dd08c01241184173d2dfea2b06ce041df5ed7
steffen <steffenosterburg@googlemail.com>
2019-05-06 11:01:37 +0200

    Added Insertion Services

---
+++ Plugins/Insertion/Services/AttributeService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Insertion\Services;
+
+class AttributeService {
+    public function createNewAttributeKey() {}
+
+    public function createNewAttributeValue() {}
+
+    public function updateAttributeKey() {}
+
+    public function updateAttributeValue() {}
+
+    public function getAttribute() {}
+
+    public function getAttributeList() {}
+
+    public function deleteAttributeKey() {}
+
+    public function deleteAttributeValue() {}
+}
