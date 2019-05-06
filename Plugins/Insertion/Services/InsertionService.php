766ff4db6c8d3d004afb4dc8c46598d15be0de80
steffen <steffenosterburg@googlemail.com>
2019-05-06 11:01:37 +0200

    Added Insertion Services

---
+++ Plugins/Insertion/Services/InsertionService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Insertion\Services;
+
+class InsertionService {
+    public function createNewInsertion() {}
+
+    public function getInsertionById() {}
+
+    public function getInsertionList() {}
+
+    public function updateInsertion() {}
+
+    public function deleteInsertion() {}
+}
---
+++ Plugins/Insertion/Services/InsertionTypeService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Insertion\Services;
+
+class InsertionTypeService {
+    public function createNewInsertionType() {
+
+    }
+
+    public function getInsertionTypeByName() {}
+
+    public function getInsertionTypeById() {}
+
+    public function getInsertionTypeList() {}
+
+    public function addAttributeToInsertionType() {}
+
+    public function removeAttributeFromInsertionType() {}
+
+    public function deleteInsertionType() {}
+}
