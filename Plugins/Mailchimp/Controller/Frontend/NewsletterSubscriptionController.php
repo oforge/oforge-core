808d9e5482250623abe803c54405ef0c75bbcec4
kevinkoessl <kk@7pkonzepte.de>
2019-05-08 15:59:18 +0200

    Merge remote-tracking branch 'upstream/dev' into dev
    # Conflicts:
    #	Plugins/Mailchimp/Bootstrap.php
    #	Plugins/Mailchimp/Controller/Frontend/NewsletterSubscriptionController.php
    #	composer.json
    #	composer.lock

--- Plugins/Mailchimp/Controller/Frontend/NewsletterSubscriptionController.php
+++ Plugins/Mailchimp/Controller/Frontend/NewsletterSubscriptionController.php
@@ -9,6 +9,8 @@ use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointAction;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointClass;
 use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
 use Oforge\Engine\Modules\I18n\Helper\I18N;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\OptimisticLockException;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -18,32 +20,40 @@ use Slim\Http\Response;
  * @package Mailchimp\Controller\Frontend
  * @EndpointClass(path="/newsletter", name="frontend_newsletter_subscription", assetScope="Frontend")
  */
-class NewsletterSubscriptionController extends AbstractController {
+class NewsletterSubscriptionController extends AbstractController
+{
 
     /**
      * @param Request $request
      * @param Response $response
+     * @throws ContainerException
      * @EndpointAction()
      */
-    public function indexAction(Request $request, Response $response) {
-        // show the email form for requesting a reset link
+    public function indexAction(Request $request, Response $response)
+    {
+        $router = Oforge()->Container()->get('router');
+
+        $subscribeLink = $router->pathFor('frontend_newsletter_subscription');
+        Oforge()->View()->assign(['subscribeLink' => $subscribeLink]);
     }
 
     /**
      * @param Request $request
      * @param Response $response
-     *
      * @return Response
      * @throws ContainerException
      * @throws ServiceNotFoundException
+     * @throws ORMException
+     * @throws OptimisticLockException
      * @EndpointAction()
      */
-    public function subscribeAction(Request $request, Response $response) {
+    public function subscribeAction(Request $request, Response $response)
+    {
         /** @var MailchimpNewsletterService $mailchimpNewsletterService */
         $mailchimpNewsletterService = Oforge()->Services()->get('mailchimp.newsletter');
         $router                     = Oforge()->Container()->get('router');
         $body                       = $request->getParsedBody();
-        $email                      = $body['email'];
+        $email                      = $body['frontend_newsletter_email'];
         $uri                        = $router->pathFor('frontend_newsletter_subscription');
 
         if (!$email) {
@@ -67,7 +77,7 @@ class NewsletterSubscriptionController extends AbstractController {
         $uri = $router->pathFor('frontend_newsletter_subscription');
 
         if (!$email) {
-            Oforge()->View()->addFlashMessage('warning', 'Invalid form data.');
+            Oforge()->View()->Flash()->addMessage('warning', I18N::translate('form_invalid_data', 'Invalid form data.'));
 
             return $response->withRedirect($uri, 302);
         } else {
@@ -75,5 +85,4 @@ class NewsletterSubscriptionController extends AbstractController {
             return $response->withRedirect($uri, 302);
         }
     }
-
 }
