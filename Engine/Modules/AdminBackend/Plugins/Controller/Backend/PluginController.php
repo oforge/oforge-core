5d5a29b14ee66c5992f2e551134a93a5375bba2d
FK <fk@7pkonzepte.de>
2019-05-12 15:59:34 +0200

    PluginCRUD - add Buttons for action links & Controller action method skeletons   ((de)avtivate,(de)install, reinstall, reinstall & activate) - disable create, view, update crud actions

--- Engine/Modules/AdminBackend/Plugins/Controller/Backend/PluginController.php
+++ Engine/Modules/AdminBackend/Plugins/Controller/Backend/PluginController.php
@@ -2,11 +2,22 @@
 
 namespace Oforge\Engine\Modules\AdminBackend\Plugins\Controller\Backend;
 
+use Exception;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointAction;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointClass;
+use Oforge\Engine\Modules\Core\Exceptions\CouldNotActivatePluginException;
+use Oforge\Engine\Modules\Core\Exceptions\PluginAlreadyActivatedException;
+use Oforge\Engine\Modules\Core\Exceptions\PluginNotFoundException;
+use Oforge\Engine\Modules\Core\Exceptions\PluginNotInstalledException;
+use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
+use Oforge\Engine\Modules\Core\Exceptions\TemplateNotFoundException;
 use Oforge\Engine\Modules\Core\Models\Plugin\Plugin;
+use Oforge\Engine\Modules\Core\Services\PluginStateService;
 use Oforge\Engine\Modules\CRUD\Controller\Backend\BaseCrudController;
 use Oforge\Engine\Modules\CRUD\Enum\CrudDataTypes;
+use Oforge\Engine\Modules\TemplateEngine\Core\Exceptions\InvalidScssVariableException;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 /**
  * Class PluginController
@@ -31,15 +42,15 @@ class PluginController extends BaseCrudController {
             'type'  => CrudDataTypes::STRING,
             'label' => ['key' => 'crud_plugin_name', 'default' => 'Plugin name'],
             'crud'  => [
-                'index'  => 'readonly',
+                'index' => 'readonly',
             ],
         ],
         [
-            'name'  => 'action',
+            'name'     => 'action',
             'type'     => CrudDataTypes::CUSTOM,
-            'label' => ['key' => 'crud_plugin_action', 'default' => 'Action'],
-            'crud'  => [
-                'index'  => 'readonly',
+            'label'    => ['key' => 'crud_plugin_action', 'default' => 'Action'],
+            'crud'     => [
+                'index' => 'readonly',
             ],
             'renderer' => [
                 'custom' => 'Backend/Plugin/Index/ActionColumn.twig',
@@ -60,10 +71,118 @@ class PluginController extends BaseCrudController {
     }
 
     /**
-     * @EndpointAction(path="/{id:\d+}/activate")
+     * @EndpointAction(path="/activate/{name}")
      */
-    public function activateAction() {
+    public function activateAction(Request $request, Response $response, array $args) {
+        if (!isset($args['name'])) {
+            // return RedirectHelper::redirect($response, 'backend_plugins');
+        }
+        $pluginName = $args['name'];
+        /** @var PluginStateService $pluginStateService */
+        $pluginStateService = Oforge()->Services()->get('plugin.state');
 
+        try {
+            // $pluginStateService->activate($pluginName);
+        } catch (PluginNotFoundException $exception) {
+        } catch (PluginNotInstalledException $exception) {
+        } catch (CouldNotActivatePluginException $exception) {
+        } catch (PluginAlreadyActivatedException $exception) {
+        } catch (TemplateNotFoundException | InvalidScssVariableException $exception) {
+
+        } catch (Exception $exception) {
+        }
+
+        var_dump($pluginName);
+        die();
+        // Oforge()->View()->assign(['test' => $pluginID]);
+        // TODO
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @throws ServiceNotFoundException
+     * @EndpointAction(path="/deactivate/{name}")
+     */
+    public function deactivateAction(Request $request, Response $response, array $args) {
+        /** @var PluginStateService $pluginStateService */
+        $pluginStateService = Oforge()->Services()->get('plugin.state');
+        //TODO
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @throws ServiceNotFoundException
+     * @EndpointAction(path="/deinstall/{name}")
+     */
+    public function deinstallAction(Request $request, Response $response, array $args) {
+        /** @var PluginStateService $pluginStateService */
+        $pluginStateService = Oforge()->Services()->get('plugin.state');
+        //TODO
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @throws ServiceNotFoundException
+     * @EndpointAction(path="/install/{name}")
+     */
+    public function installAction(Request $request, Response $response, array $args) {
+        /** @var PluginStateService $pluginStateService */
+        $pluginStateService = Oforge()->Services()->get('plugin.state');
+        //TODO
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @throws ServiceNotFoundException
+     * @EndpointAction(path="/reinstall/{name}")
+     */
+    public function reinstallAction(Request $request, Response $response, array $args) {
+        /** @var PluginStateService $pluginStateService */
+        $pluginStateService = Oforge()->Services()->get('plugin.state');
+        //TODO
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @throws ServiceNotFoundException
+     * @EndpointAction(path="/reinstall-activate/{name}")
+     */
+    public function reinstallActivateAction(Request $request, Response $response, array $args) {
+        /** @var PluginStateService $pluginStateService */
+        $pluginStateService = Oforge()->Services()->get('plugin.state');
+        //TODO
+    }
+
+    /** @EndpointAction(create=false) */
+    public function updateAction(Request $request, Response $response, array $args) {
+    }
+
+    /** @EndpointAction(create=false) */
+    public function createAction(Request $request, Response $response) {
+    }
+
+    /** @EndpointAction(create=false) */
+    public function viewAction(Request $request, Response $response, array $args) {
+    }
+
+    /** @EndpointAction(path="/delete/{name}") */
+    public function deleteAction(Request $request, Response $response, array $args) {
+        //TODO
     }
 
 }
