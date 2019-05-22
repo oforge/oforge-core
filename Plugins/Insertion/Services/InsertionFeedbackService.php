afec4c2e04dfa5770664215a5c64e43153032ff2
Matthäus <ms@7pkonzepte.de>
2019-05-22 10:10:42 +0200

    Worked on insertion creation

---
+++ Plugins/Insertion/Services/InsertionFeedbackService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Insertion\Services;
+
+use Insertion\Models\InsertionFeedback;
+use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
+
+class InsertionFeedbackService extends AbstractDatabaseAccess {
+    public function __construct() {
+        parent::__construct([
+            'default' => InsertionFeedback::class,
+        ]);
+    }
+
+    public function savePostData() {
+        if (!empty($_POST)) {
+
+            $data     = ["rating" => $_POST["feedback_rating"], "text" => $_POST["feedback_text"]];
+            $feedback = InsertionFeedback::create($data);
+            $this->entityManager()->persist($feedback);
+            $this->entityManager()->flush();
+        }
+    }
+}
