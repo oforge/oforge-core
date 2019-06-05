47b52b5aae1fc4fce30b181e3f6b45cd139c1902
mys3lf <matthaeus.gdaniec@gmx.de>
2019-06-05 15:22:43 +0200

    Worked on insertion backend crud service

--- Plugins/Insertion/Controller/Frontend/FrontendInsertionController.php
+++ Plugins/Insertion/Controller/Frontend/FrontendInsertionController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Event\OnFlushEventArgs;
 use FrontendUserManagement\Abstracts\SecureFrontendController;
 use FrontendUserManagement\Models\User;
 use FrontendUserManagement\Services\FrontendUserService;
+use Insertion\Models\Insertion;
 use Insertion\Models\InsertionType;
 use Insertion\Models\InsertionTypeAttribute;
 use Insertion\Services\InsertionCreatorService;
@@ -16,10 +17,13 @@ use Insertion\Services\InsertionProfileService;
 use Insertion\Services\InsertionService;
 use Insertion\Services\InsertionTypeService;
 use Insertion\Services\InsertionUpdaterService;
+use Oforge\Engine\Modules\Auth\Models\User\BackendUser;
+use Oforge\Engine\Modules\Auth\Services\AuthService;
 use Oforge\Engine\Modules\CMS\Abstracts\AbstractContentType;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointAction;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointClass;
 use Oforge\Engine\Modules\Core\Helper\StringHelper;
+use function PHPSTORM_META\type;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Router;
@@ -249,11 +253,28 @@ class FrontendInsertionController extends SecureFrontendController {
         /**
          * @var $service InsertionService
          */
-        $service   = Oforge()->Services()->get("insertion");
+        $service = Oforge()->Services()->get("insertion");
+        /**
+         * @var $insertion Insertion
+         */
         $insertion = $service->getInsertionById(intval($id));
 
         if (!isset($insertion) || $insertion == null) {
             return $response->withRedirect("/404", 301);
+        }
+
+        if (!($insertion->isActive() && $insertion->isModeration())) {
+            if (isset($_SESSION['auth'])) {
+                $auth = $_SESSION['auth'];
+            }
+
+            /** @var AuthService $authService */
+            $authService = Oforge()->Services()->get('auth');
+            $user        = $authService->decode($auth);
+
+            if ($user["type"] != BackendUser::class) {
+                return $response->withRedirect("/404", 301);
+            }
         }
 
         Oforge()->View()->assign(["insertion" => $insertion->toArray(3, ['user' => ['*', 'id']])]);
