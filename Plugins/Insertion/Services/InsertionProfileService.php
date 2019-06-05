aaf67699bebfea994b8ece56a8ca8220a63159b9
mys3lf <matthaeus.gdaniec@gmx.de>
2019-06-05 14:00:26 +0200

    Worked on insertion profile update

--- Plugins/Insertion/Services/InsertionProfileService.php
+++ Plugins/Insertion/Services/InsertionProfileService.php
@@ -8,17 +8,36 @@ use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
 class InsertionProfileService extends AbstractDatabaseAccess {
     public function __construct() {
         parent::__construct([
-            'default'                 => InsertionProfile::class,
+            'default' => InsertionProfile::class,
         ]);
     }
 
+    /**
+     * @param int $user
+     *
+     * @return InsertionProfile|null
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function get(int $user) : ?InsertionProfile {
+        /**
+         * @var $result InsertionProfile
+         */
+        $result = $this->repository()->findOneBy(["user" => $user]);
 
-    public function get(int $user) {
-
+        return $result;
     }
 
     public function update(int $user, array $params) {
+        /**
+         * @var $result InsertionProfile
+         */
+        $result = $this->repository()->findOneBy(["user" => $user]);
 
+        if($result != null) {
+
+        } else {
+
+        }
     }
 
 }
