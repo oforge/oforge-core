755d86b0d5b97c58699db5debcaa110cf8328e1f
Steffen <steffenosterburg@googlemail.com>
2019-06-07 15:05:36 +0200

    Insertion Report page

--- Plugins/Insertion/Bootstrap.php
+++ Plugins/Insertion/Bootstrap.php
@@ -88,6 +88,7 @@ class Bootstrap extends AbstractBootstrap {
         $this->dependencies = [
             \FrontendUserManagement\Bootstrap::class,
             \Messenger\Bootstrap::class,
+            \Helpdesk\Bootstrap::class,
         ];
     }
 
