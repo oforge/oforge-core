780675df6c6621ba54b53e7934ae3b9cc7db6367
steffen <steffenosterburg@googlemail.com>
2019-06-12 15:34:12 +0200

    Added CRUD for Seo url's

--- Plugins/Seo/Bootstrap.php
+++ Plugins/Seo/Bootstrap.php
@@ -2,9 +2,11 @@
 
 namespace Seo;
 
+use Oforge\Engine\Modules\AdminBackend\Core\Services\BackendNavigationService;
 use Oforge\Engine\Modules\CMS\Services\ContentTypeManagementService;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractBootstrap;
 use Oforge\Engine\Modules\TemplateEngine\Core\Services\TemplateRenderService;
+use Seo\Controller\Backend\BackendSeoUrlController;
 use Seo\Middleware\SeoMiddleware;
 use Seo\Models\SeoUrl;
 use Seo\Services\SeoService;
@@ -21,6 +23,10 @@ class Bootstrap extends AbstractBootstrap {
      * Bootstrap constructor.
      */
     public function __construct() {
+        $this->endpoints = [
+            BackendSeoUrlController::class,
+        ];
+
         $this->models = [
             SeoUrl::class,
         ];
@@ -43,4 +49,18 @@ class Bootstrap extends AbstractBootstrap {
 
         $templateRenderer->View()->addExtension(new SeoExtension());
     }
+
+    public function activate() {
+        /** @var BackendNavigationService $sidebarNavigation */
+        $sidebarNavigation = Oforge()->Services()->get('backend.navigation');
+        $sidebarNavigation->put([
+            'name'     => 'backend_seo',
+            'order'    => 5,
+            'parent'   => 'backend_content',
+            'icon'     => 'fa fa-external-link-square',
+            'path'     => 'backend_seo',
+            'position' => 'sidebar',
+        ]);
+    }
+
 }
