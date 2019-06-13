f06bdc9073bd166e9686b61aa81e77dce83bfc07
mys3lf <matthaeus.gdaniec@gmx.de>
2019-06-13 12:54:03 +0200

    Implemented multilanguage support

--- Engine/Modules/CMS/Controller/Frontend/PageController.php
+++ Engine/Modules/CMS/Controller/Frontend/PageController.php
@@ -24,8 +24,7 @@ use Slim\Router;
  * @package Oforge\Engine\Modules\CMS\Controller\Frontend
  * @EndpointClass(path="/[{content:.*}]", name="frontend_page", assetScope="Frontend", order=99999)
  */
-class PageController extends AbstractController
-{
+class PageController extends AbstractController {
 
     /**
      * @param Request $request
@@ -36,30 +35,52 @@ class PageController extends AbstractController
      * @throws ORMException
      * @EndpointAction()
      */
-    public function indexAction(Request $request, Response $response)
-    {
+    public function indexAction(Request $request, Response $response) {
         /**
          * @var PageService $pagePathService
          */
         $pagePathService = Oforge()->Services()->get('page.path');
-        $path = $request->getUri()->getPath();
+        $path            = $request->getUri()->getPath();
 
         $cmsContent = $pagePathService->loadContentForPagePath($path);
-        $pagePath = $pagePathService->getPagePath($path);
+        $pagePath   = $pagePathService->getPagePath($path);
+
+        $data     = Oforge()->View()->fetch();
+        $language = $data["meta"]["language"];
+
+        if ($cmsContent == null) {
+            $path = "/" . $language . $path;
+
+            $cmsContent = $pagePathService->loadContentForPagePath($path);
+            $pagePath   = $pagePathService->getPagePath($path);
+        }
 
         if ($cmsContent !== null) {
+            if ($pagePath->getLanguage()->getIso() != $language) {
+                foreach ($pagePath->getPage()->getPaths() as $path) {
+                    if ($path->getLanguage()->getIso() == $language) {
+                        $response = $response->withRedirect($path->getPath(), 301);
+                    }
+                }
+            }
+
             // TODO: Remove meta assignment
-            Oforge()->View()->assign(['content' => $cmsContent, "cms" => $pagePath->toArray(), 'meta'=> ["header_class" => "cms cms-page " . $pagePath->getPage()->getName()]]);
+            Oforge()->View()->assign([
+                'content' => $cmsContent,
+                "cms"     => $pagePath->toArray(),
+                'meta'    => ["header_class" => "cms cms-page " . $pagePath->getPage()->getName()],
+            ]);
 
             return $response;
         }
 
         /** @var Router $router */
-        $router = Oforge()->App()->getContainer()->get('router');
-        $uri = $router->pathFor('not_found');
+        $router   = Oforge()->App()->getContainer()->get('router');
+        $uri      = $router->pathFor('not_found');
         $response = $response->withRedirect($uri, 301);
 
         return $response;
+
     }
 
 }
