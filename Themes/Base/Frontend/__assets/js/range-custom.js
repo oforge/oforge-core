f0f18c84de114dabc2d88839dc7e23b7acca6906
Der-Alex <alexander.thimm@googlemail.com>
2019-06-14 14:36:14 +0200

    range slider and input with unit label

--- Themes/Base/Frontend/__assets/js/range-custom.js
+++ Themes/Base/Frontend/__assets/js/range-custom.js
@@ -4,30 +4,34 @@ if (typeof Oforge !== 'undefined') {
         selector: '[data-range-slider]',
         init: function() {
             var self = this;
-
             var rangeSlider = document.querySelectorAll(self.selector);
-            debugger;
 
             rangeSlider.forEach(function (item) {
                 var firstItem = item.querySelector('[data-range-from]');
                 var secondItem = item.querySelector('[data-range-to]');
                 var firstItemName = firstItem.dataset.rangeName;
                 var secondItemName = secondItem.dataset.rangeName;
+                var rangeFrom = parseFloat(firstItem.getAttribute('min'));
+                var rangeTo = parseFloat(firstItem.getAttribute('max'));
 
-                var priceRange = new JSR(['[data-range-name="' + firstItemName + '"]', '[data-range-name="' + secondItemName + '"]'], {
+                var range = new JSR(['[data-range-name="' + firstItemName + '"]', '[data-range-name="' + secondItemName + '"]'], {
                     sliders: 2,
-                    min: firstItem.getAttribute('min'),
-                    max: secondItem.getAttribute('max'),
+                    min: rangeFrom,
+                    max: rangeTo,
                     values: [firstItem.getAttribute('value'),
                         secondItem.getAttribute('value')],
                     limit: {
-                        show: true
+                        show: false
                     },
-                    labels: false,
+                    modules: {
+                        labels: false
+                    },
                     grid: false
+                }).addEventListener('update', function(input, value) {
+                    console.log('Custom events test: New value set: ' + input + ' ' + value);
                 });
 
-                priceRange.addEventListener('update', function(input, value) {
+                range.addEventListener('update', function(input, value) {
                     if (input.matches('[data-range-from]')) {
                         firstItem.setAttribute('value', value);
                     }
