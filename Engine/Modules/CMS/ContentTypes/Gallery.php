5796007ffd84356003418ddf93fc634f11c8b4ad
FK <fk@7pkonzepte.de>
2019-06-24 11:47:48 +0200

    CMS - ContentType Gallery (indev)

---
+++ Engine/Modules/CMS/ContentTypes/Gallery.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Oforge\Engine\Modules\CMS\ContentTypes;
+
+use Oforge\Engine\Modules\CMS\Abstracts\AbstractContentType;
+use Oforge\Engine\Modules\CMS\Models\Content\Content;
+
+/**
+ * Class Gallery
+ *
+ * @package Oforge\Engine\Modules\CMS\ContentTypes
+ */
+class Gallery extends AbstractContentType {
+
+    /**
+     * Return whether or not content type is a container type like a row
+     *
+     * @return bool true|false
+     */
+    public function isContainer() : bool {
+        return false;
+    }
+
+    /**
+     * Return edit data for page builder of content type
+     *
+     * @return array
+     */
+    public function getEditData() {
+        $data           = [];
+        $data['id']     = $this->getContentId();
+        $data['type']   = $this->getId();
+        $data['name']   = $this->getContentName();
+        $data['css']    = $this->getContentCssClass();
+        $data['url']    = $this->getContentData();
+        $data['images'] = $this->getContentData();
+
+        return $data;
+    }
+public function getContentData() {
+        return [1,2];
+}
+
+    /**
+     * Set edit data for page builder of content type
+     *
+     * @param $data
+     *
+     * @return Gallery $this
+     */
+    public function setEditData($data) {
+        $this->setContentData($data['images']);
+
+        return $this;
+    }
+
+    /**
+     * Return data for page rendering of content type
+     *
+     * @return array
+     */
+    public function getRenderData() {
+        $data                = [];
+        $data['form']        = 'ContentTypes/' . $this->getPath() . '/PageBuilderForm.twig';
+        $data['type']        = 'ContentTypes/' . $this->getPath() . '/PageBuilderPreview.twig';
+        $data['typeId']      = $this->getId();
+        $data['isContainer'] = $this->isContainer();
+        $data['css']         = $this->getContentCssClass();
+        $data['images']      = $this->getContentData();
+
+        return $data;
+    }
+
+    /**
+     * Create a child of given content type
+     *
+     * @param Content $contentEntity
+     * @param int $order
+     *
+     * @return Gallery $this
+     */
+    public function createChild($contentEntity, $order) {
+        return $this;
+    }
+
+    /**
+     * Delete a child
+     *
+     * @param Content $contentEntity
+     * @param int $order
+     *
+     * @return Gallery $this
+     */
+    public function deleteChild($contentEntity, $order) {
+        return $this;
+    }
+
+    /**
+     * Return child data of content type
+     *
+     * @return array|false should return false if no child content data is available
+     */
+    public function getChildData() {
+        return false;
+    }
+}
