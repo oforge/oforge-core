6806a020b7f5090de581dda10a2514b1dcfe003e
mys3lf <matthaeus.gdaniec@gmx.de>
2019-07-11 21:03:43 +0200

    Added month and year filter types

--- Plugins/Insertion/Enum/AttributeType.php
+++ Plugins/Insertion/Enum/AttributeType.php
@@ -7,6 +7,8 @@ abstract class AttributeType {
     const SINGLE    = 'single';
     const RANGE     = 'range';
     const DATE     = 'date';
+    const DATEYEAR     = 'date-year';
+    const DATEMONTH     = 'date-month';
     const BOOLEAN   = 'boolean';
     const NUMBER    = 'number';
     const TEXT      = 'text';
