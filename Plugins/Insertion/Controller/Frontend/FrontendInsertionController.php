87abea3d0461e26ce43280fdf7df24dfd6cb163c
mys3lf <matthaeus.gdaniec@gmx.de>
2019-07-11 16:04:39 +0200

    Added attributes to detail page

--- Plugins/Insertion/Controller/Frontend/FrontendInsertionController.php
+++ Plugins/Insertion/Controller/Frontend/FrontendInsertionController.php
@@ -381,10 +381,22 @@ class FrontendInsertionController extends SecureFrontendController {
         /** @var $insertionTypeService InsertionTypeService */
         $insertionTypeService = Oforge()->Services()->get("insertion.type");
 
-        $typeAttributes = $insertionTypeService->getInsertionTypeAttributeTree($insertion->getInsertionType()->getId());
-
+        $typeAttributes  = $insertionTypeService->getInsertionTypeAttributeTree($insertion->getInsertionType()->getId());
+        $insertionValues = [];
+        foreach ($insertion->getValues() as $value) {
+            if (isset($insertionValues[$value->getAttributeKey()->getId()])) {
+                if (is_array($insertionValues[$value->getAttributeKey()->getId()])) {
+                    $insertionValues[$value->getAttributeKey()->getId()][] = $value->getValue();
+                } else {
+                    $insertionValues[$value->getAttributeKey()->getId()] = [$insertionValues[$value->getAttributeKey()->getId()], $value->getValue()];
+                }
+            } else {
+                $insertionValues[$value->getAttributeKey()->getId()] = $value->getValue();
+            }
+        }
         Oforge()->View()->assign(["attributes" => $typeAttributes]);
         Oforge()->View()->assign(["all_attributes" => $insertionTypeService->getInsertionTypeAttributeMap()]);
+        Oforge()->View()->assign(["insertion_values" => $insertionValues]);
 
     }
 
