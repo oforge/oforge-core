6d88b6c9db516fe1f9ce46c9e39f69ce77fa512a
mys3lf <matthaeus.gdaniec@gmx.de>
2019-07-16 15:43:22 +0200

    Several ui changes Added some permission

--- Plugins/Insertion/Controller/Backend/BackendInsertionTypeController.php
+++ Plugins/Insertion/Controller/Backend/BackendInsertionTypeController.php
@@ -9,6 +9,7 @@ use Insertion\Models\InsertionTypeAttribute;
 use Insertion\Services\AttributeService;
 use Insertion\Services\InsertionTypeService;
 use Oforge\Engine\Modules\AdminBackend\Core\Abstracts\SecureBackendController;
+use Oforge\Engine\Modules\Auth\Models\User\BackendUser;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointAction;
 use Oforge\Engine\Modules\Core\Annotation\Endpoint\EndpointClass;
 use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
@@ -199,6 +200,10 @@ class BackendInsertionTypeController extends SecureBackendController {
     }
 
     public function initPermissions() {
+        $this->ensurePermissions('deleteAction', BackendUser::class, BackendUser::ROLE_MODERATOR);
+        $this->ensurePermissions('indexAction', BackendUser::class, BackendUser::ROLE_MODERATOR);
+        $this->ensurePermissions('editAction', BackendUser::class, BackendUser::ROLE_MODERATOR);
+
         parent::initPermissions();
     }
 }
