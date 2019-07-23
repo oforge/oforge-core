90eea778bb816ec2890ba45cbcb01f6eac9006b5
Basti <sk@7pkonzepte.de>
2019-07-23 11:25:55 +0200

    fixed some stuff, mailer nickname and sender name

--- Plugins/FrontendUserManagement/Controller/Frontend/RegistrationController.php
+++ Plugins/FrontendUserManagement/Controller/Frontend/RegistrationController.php
@@ -190,6 +190,7 @@ class RegistrationController extends AbstractController {
         $templateData = [
             'activationLink' => $activationLink,
             'user_mail'      => $user['email'],
+            'sender_mail'         => $mailService->getSenderAddress('info'),
             // TODO: 'receiver_name'  => ???
         ];
 
