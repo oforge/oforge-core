c2af730b1d1151c085fd73e96e2cc6cce9fbb5c8
FK <fk@7pkonzepte.de>
2019-07-23 18:28:05 +0200

    Refactoring of SecureMiddlewares - add user data to view independent of permission - add flash message for redirect - add userClass & default permission role of controller does not extends     SecureController or implements ensurePermission - configurable view-userdata-key -

--- Engine/Modules/AdminBackend/Core/Middleware/BackendSecureMiddleware.php
+++ Engine/Modules/AdminBackend/Core/Middleware/BackendSecureMiddleware.php
@@ -3,8 +3,18 @@
 namespace Oforge\Engine\Modules\AdminBackend\Core\Middleware;
 
 use Oforge\Engine\Modules\Auth\Middleware\SecureMiddleware;
+use Oforge\Engine\Modules\Auth\Models\User\BackendUser;
 
-class BackendSecureMiddleware extends SecureMiddleware
-{
+/**
+ * Class BackendSecureMiddleware
+ *
+ * @package Oforge\Engine\Modules\AdminBackend\Core\Middleware
+ */
+class BackendSecureMiddleware extends SecureMiddleware {
+    /** @var string $userClass */
+    protected $userClass = BackendUser::class;
+    /** @var string $fallbackPermissionRole */
+    protected $fallbackPermissionRole = BackendUser::ROLE_MODERATOR;
+    /** @var string $urlPathName */
     protected $urlPathName = 'backend_login';
 }
--- Engine/Modules/Auth/Middleware/SecureMiddleware.php
+++ Engine/Modules/Auth/Middleware/SecureMiddleware.php
@@ -2,10 +2,13 @@
 
 namespace Oforge\Engine\Modules\Auth\Middleware;
 
+use Oforge\Engine\Modules\Auth\Controller\SecureController;
+use Oforge\Engine\Modules\Auth\Models\User\BaseUser;
 use Oforge\Engine\Modules\Auth\Services\AuthService;
 use Oforge\Engine\Modules\Auth\Services\PermissionService;
 use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
 use Oforge\Engine\Modules\Core\Helper\RouteHelper;
+use Oforge\Engine\Modules\I18n\Helper\I18N;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -15,6 +18,12 @@ use Slim\Http\Response;
  * @package Oforge\Engine\Modules\Auth\Middleware
  */
 class SecureMiddleware {
+    /** @var string $userClass */
+    protected $userClass = BaseUser::class;
+    /** @var string $fallbackPermissionRole */
+    protected $fallbackPermissionRole = 9999;
+    /** @var string $viewUserDataKey */
+    protected $viewUserDataKey = 'user';
     /** @var string $urlPathName The named path for redirects */
     protected $urlPathName = '';
 
@@ -27,67 +36,66 @@ class SecureMiddleware {
      * @return Response|null
      * @throws ServiceNotFoundException
      */
-    public function prepend($request, $response) : ?Response {
-        $routeController  = Oforge()->View()->get('meta')['route'];
-        $controllerClass  = $routeController['controllerClass'];
-        $controllerMethod = $routeController['controllerMethod'];
-
-        /**
-         * @var $permissionService PermissionService
-         */
-        $permissionService = Oforge()->Services()->get('permissions');
-
-        $permissions = $permissionService->get($controllerClass, $controllerMethod);
-        $auth        = null;
-
+    public function prepend(Request $request, Response $response) : ?Response {
         if (isset($_SESSION['auth'])) {
             $auth = $_SESSION['auth'];
-        }
-
-        if (isset($permissions)) {
             /** @var AuthService $authService */
             $authService = Oforge()->Services()->get('auth');
             $user        = $authService->decode($auth);
-
-            if ($this->isUserValid($user, $permissions)) {
+            if ($user !== null && $user['type'] === $this->userClass) {
                 Oforge()->View()->assign([
-                    'user' => $user,
+                    $this->viewUserDataKey => $user,
                 ]);
-                //nothing to do. proceed
-            } else {
-                /*
-                TODO: If there is a secured area, there should be either a message when redirecting
-                      or a 401 status code.
-                */
-
-                Oforge()->View()->assign(['stopNext' => true]);
-
-                $currentPath = $request->getUri()->getPath();
-                $_SESSION["login_redirect_url"] = $currentPath;
-
-                if (!empty($this->urlPathName)) {
-                    return RouteHelper::redirect($response, $this->urlPathName);
-                }
-
-                return $response = $response->withRedirect('/', 302);
             }
+        }
+        $routeController  = Oforge()->View()->get('meta')['route'];
+        $controllerClass  = $routeController['controllerClass'];
+        $controllerMethod = $routeController['controllerMethod'];
+        if (is_subclass_of($controllerClass, SecureController::class)) {
+            /** @var PermissionService $permissionService */
+            $permissionService = Oforge()->Services()->get('permissions');
+            $permission        = $permissionService->get($controllerClass, $controllerMethod);
+        } else {
+            $permission = ['role' => $this->fallbackPermissionRole, 'type' => $this->userClass];
+        }
+        if ($this->isUserValid($user, $permission)) {
+            //nothing to do. proceed
+        } else {
+            Oforge()->View()->assign(['stopNext' => true]);
+            $_SESSION["login_redirect_url"] = $request->getUri()->getPath();
+            $this->createPermissionDeniedFlashMessage();
+            if (!empty($this->urlPathName)) {
+                return RouteHelper::redirect($response, $this->urlPathName);
+            }
+
+            return $response->withRedirect('/', 302);
         }
 
         return $response;
     }
 
     /**
-     * @param $user
-     * @param $permissions
+     * Create Flash message for failed request.
+     */
+    protected function createPermissionDeniedFlashMessage() {
+        Oforge()->View()->Flash()->addMessage('error', I18N::translate('secured_area_no_permission', [
+            'en' => 'You do not have permission for this page. Log in with another user.',
+            'de' => 'Du hast keine Berechtigung für diese Seite. Logge dich mit einem anderem User ein.',
+        ]));
+    }
+
+    /**
+     * @param array|null $user
+     * @param array $permission
      *
      * @return bool
      */
-    protected function isUserValid($user, $permissions) {
-        return (!is_null($user)
+    protected function isUserValid(?array $user, array $permission) {
+        return ($user !== null
                 && isset($user['role'])
-                && $user['role'] <= $permissions['role']
                 && isset($user['type'])
-                && $user['type'] == $permissions['type']);
+                && $user['type'] === $permission['type']
+                && $user['role'] <= $permission['role']);
     }
 
 }
