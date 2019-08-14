742b02c18014e9ef6d6da995b7755b0d3ee5bff8
FK <fk@7pkonzepte.de>
2019-08-14 23:23:43 +0200

    Installation (uninstall, ...) of Insertion backend dashboard widgets

--- Plugins/Insertion/Widgets/InsertionModerationWidget.php
+++ Plugins/Insertion/Widgets/InsertionModerationWidget.php
@@ -9,17 +9,25 @@
 namespace Insertion\Widgets;
 
 use Insertion\Models\Insertion;
+use Oforge\Engine\Modules\AdminBackend\Core\Abstracts\DashboardWidgetInterface;
 use Oforge\Engine\Modules\Core\Abstracts\AbstractDatabaseAccess;
 
-class InsertionModerationWidget extends AbstractDatabaseAccess implements \Oforge\Engine\Modules\AdminBackend\Core\Abstracts\DashboardWidgetInterface {
+/**
+ * Class InsertionModerationWidget
+ *
+ * @package Insertion\Widgets
+ */
+class InsertionModerationWidget extends AbstractDatabaseAccess implements DashboardWidgetInterface {
 
     public function __construct() {
-        parent::__construct(["default" => Insertion::class]);
+        parent::__construct(Insertion::class);
     }
 
+    /** @inheritDoc */
     function prepareData() : array {
-        $users = $this->repository()->count(["deleted" => false, "moderation" => false]);
+        $users = $this->repository()->count(['deleted' => false, 'moderation' => false]);
 
-        return ["count" => $users];
+        return ['count' => $users];
     }
+
 }
