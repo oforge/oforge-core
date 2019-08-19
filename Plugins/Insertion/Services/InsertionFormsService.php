eeb302c74da9bfb6323a10c8663416e604e8e315
mys3lf <matthaeus.gdaniec@gmx.de>
2019-08-19 15:37:15 +0200

    Fixed bug

--- Plugins/Insertion/Services/InsertionFormsService.php
+++ Plugins/Insertion/Services/InsertionFormsService.php
@@ -35,13 +35,16 @@ class InsertionFormsService extends AbstractDatabaseAccess {
     public function processPostData($sessionKey) : ?array {
         $prefix = null;
         if (!isset($_SESSION['insertion' . $sessionKey])) {
-            $_SESSION['insertion' . $sessionKey] = [];
+            $_SESSION['insertion' . $sessionKey]              = [];
+            $_SESSION['insertion' . $sessionKey]["insertion"] = [];
         }
 
-        $insertion = array_merge($_SESSION['insertion' . $sessionKey]["insertion"], $_POST["insertion"]);
+        $insertion = $_SESSION['insertion' . $sessionKey]["insertion"];
+        if (isset($_POST["insertion"])) {
+            $insertion = array_merge($_SESSION['insertion' . $sessionKey]["insertion"], $_POST["insertion"]);
+        }
 
         $_SESSION['insertion' . $sessionKey] = array_merge($_SESSION['insertion' . $sessionKey], $_POST);
-
         $_SESSION['insertion' . $sessionKey]["insertion"] = $insertion;
 
         /** @var MediaService $mediaService */
@@ -142,7 +145,7 @@ class InsertionFormsService extends AbstractDatabaseAccess {
                 "phone"   => $pageData["contact_phone"],
                 "zip"     => $pageData["contact_zip"],
                 "city"    => $pageData["contact_city"],
-                "visible" => isset($pageData["contact_visible"]) && !empty($pageData["contact_visible"]) && $pageData["contact_visible"] != "off" ,
+                "visible" => isset($pageData["contact_visible"]) && !empty($pageData["contact_visible"]) && $pageData["contact_visible"] != "off",
             ],
             "content"             => [
                 "language"    => $language,
