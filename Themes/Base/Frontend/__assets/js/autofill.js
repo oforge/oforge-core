e4596219131fd4c1327e48b102531394c0970007
kevinkoessl <kevin.koessl@gmail.com>
2019-08-28 15:01:32 +0200

    Added caching to autofill JS Plugin.

--- Themes/Base/Frontend/__assets/js/autofill.js
+++ Themes/Base/Frontend/__assets/js/autofill.js
@@ -1,3 +1,27 @@
+/*Example Usage:
+use any input types that you want to be autofilled
+    <input type="text"
+
+    //turn default autocomplete off
+    autocomplete="off"
+
+    //selector the javascript plugin
+    data-autofill="true"
+
+    //define the endpoint from which the autofill-data should be fetched
+    data-autofill-endpoint="url"
+
+    //define an additional array of attribute keys from which you want the values to be fetched
+    data-autofill-attribute-keys="[1, 2, 3, ...]"
+
+    //minimum amount of characters to be typed to suggest autofill-values
+    data-autofill-min-characters="2"
+
+    //placeholder to show, when no suggestions where found
+    data-autofill-placeholder='No Suggestions'
+    />
+*/
+
 ;(function () {
     if (typeof Oforge !== 'undefined') {
         Oforge.register({
@@ -5,27 +29,32 @@
             selector: '[data-autofill]',
             init: function () {
                 let self = this;
+                let cache = {}, processing = {}, requestAttempts = {};
+
                 let classNames = {
                     autofill: 'autofill',
                     autofillItems: 'autofill__items',
                     autofillOption: 'autofill__option',
+                    autofillPlaceholder: 'autofill__placeholder',
                     autofillActive: 'option--active',
                 };
+
                 let selectors = {
-                    select: '.' + classNames.select,
-                    selectIsOpen: '.' + classNames.selectIsOpen,
-                    selectText: '.' + classNames.selectText,
-                    selectItem: '.' + classNames.selectItem,
-                    selectItemIsChecked: '.' + classNames.selectItemIsChecked,
-                    selectRequireInput: '.' + classNames.selectRequireInput,
-                    selectValue: '.' + classNames.selectValue,
-                    subSelect: '[data-sub-select]',
-                    selectFilter: '[data-select-filter]',
-                    noSubSelect: '.select:not(.select--is-sub)[data-sortable]'
+                    input: 'input',
+                    div: 'div'
                 };
 
                 let defaults = {
-                    minCharacters: 3
+                    minCharacters: 3,
+                    maxIds: 65536,
+                    maxRequestAttempts: 50,
+                    requestWaitingTime: 100
+                };
+
+                let keycodes = {
+                    down: 40,
+                    up: 38,
+                    enter: 13
                 };
 
                 let autofillIds = [];
@@ -35,10 +64,12 @@
 
                     autofillInput.addEventListener("input", function (event) {
                         let value = this.value;
+                        let suggestionsFound = false;
+                        let minCharacters = this.dataset.autofillMinCharacters !== null ? this.dataset.autofillMinCharacters : defaults.minCharacters;
+
                         //close any already open lists of autofilled values
                         closeAllLists();
 
-                        let minCharacters = this.dataset.autofillMinCharacters !== null ? this.dataset.autofillMinCharacters : defaults.minCharacters;
                         if (!value || value.length < minCharacters) {
                             return false;
                         }
@@ -46,95 +77,101 @@
 
                         //create a div element that will contain the autofill items
                         let autofillItems = document.createElement("div");
-                        autofillItems.setAttribute("id", this.id + "autofill-list");
+                        autofillItems.setAttribute("id", this.id + classNames.autofillItems);
                         autofillItems.classList.add(classNames.autofillItems);
-                        this.parentNode.appendChild(autofillItems);
-
 
                         for (let i = 0; i < autofillData.length; i++) {
                             if (autofillData[i].toUpperCase().includes(value.toUpperCase())) {
+                                suggestionsFound = true;
                                 //create a div element for each autofill option
                                 let autofillOption = document.createElement("div");
                                 autofillOption.classList.add(classNames.autofillOption);
 
                                 //emphasize matching characters
-
                                 autofillOption.innerHTML = emphasizeValueInOption(autofillData[i], value);
 
                                 //insert a input field that will hold the current array item's value
                                 autofillOption.innerHTML += "<input type='hidden' value='" + autofillData[i] + "'>";
                                 autofillOption.addEventListener("click", function (event) {
-                                    autofillInput.value = this.getElementsByTagName("input")[0].value;
+                                    autofillInput.value = this.getElementsByTagName(selectors.input)[0].value;
                                     closeAllLists();
                                 });
                                 autofillItems.appendChild(autofillOption);
                             }
                         }
+
+                        if(suggestionsFound) {
+                            this.parentNode.appendChild(autofillItems);
+                        } else if (autofillInput.dataset.autofillPlaceholder) {
+                            //append a placeholder if defined
+                            let autofillPlaceholder = document.createElement("div");
+
+                            autofillPlaceholder.innerHTML = autofillInput.dataset.autofillPlaceholder;
+                            autofillPlaceholder.classList.add(classNames.autofillPlaceholder);
+
+                            this.parentNode.appendChild(autofillItems);
+                            autofillItems.appendChild(autofillPlaceholder);
+                        }
                     });
 
                     autofillInput.addEventListener("keydown", function (event) {
-                        var x = document.getElementById(this.id + "autofill-list");
-                        if (x) x = x.getElementsByTagName("div");
-                        if (event.keyCode === 40) {
-                            /*If the arrow DOWN key is pressed,
-                            increase the currentFocus variable:*/
+                        let self = document.getElementById(this.id + classNames.autofillItems);
+                        if (self) {
+                            self = self.getElementsByTagName(selectors.div);
+                        }
+                        if (event.keyCode === keycodes.down) {
                             currentFocus++;
-                            /*and and make the current item more visible:*/
-                            addActive(x);
-                        } else if (event.keyCode === 38) { //up
-                            /*If the arrow UP key is pressed,
-                            decrease the currentFocus variable:*/
+                            addActive(self);
+                        } else if (event.keyCode === keycodes.up) {
                             currentFocus--;
-                            /*and and make the current item more visible:*/
-                            addActive(x);
-                        } else if (event.keyCode === 13) {
-                            /*If the ENTER key is pressed, prevent the form from being submitted,*/
-                            e.preventDefault();
+                            addActive(self);
+                        } else if (event.keyCode === keycodes.enter) {
+                            event.preventDefault();
                             if (currentFocus > -1) {
-                                /*and simulate a click on the "active" item:*/
-                                if (x) x[currentFocus].click();
+                                if (self) {
+                                    self[currentFocus].click();
+                                }
                             }
                         }
                     });
 
-                    function addActive(x) {
-                        /*a function to classify an item as "active":*/
-                        if (!x) return false;
-                        /*start by removing the "active" class on all items:*/
-                        removeActive(x);
-                        if (currentFocus >= x.length) currentFocus = 0;
-                        if (currentFocus < 0) currentFocus = (x.length - 1);
-                        /*add class "autocomplete-active":*/
-                        x[currentFocus].classList.add(classNames.autofillActive);
+
+                    function addActive(element) {
+                        //adds 'active' class to selected element and removes it from all remaining elements
+                        if (!element) return false;
+                        //remove 'active' class on all items
+                        removeActive(element);
+                        if (currentFocus >= element.length) currentFocus = 0;
+                        if (currentFocus < 0) currentFocus = (element.length - 1);
+                        //add 'active' class
+                        element[currentFocus].classList.add(classNames.autofillActive);
                     }
 
-                    function removeActive(x) {
-                        /*a function to remove the "active" class from all autocomplete items:*/
-                        for (var i = 0; i < x.length; i++) {
-                            x[i].classList.remove(classNames.autofillActive);
+                    function removeActive(element) {
+                        //removes active class from element
+                        for (let i = 0; i < element.length; i++) {
+                            element[i].classList.remove(classNames.autofillActive);
                         }
                     }
 
                     function closeAllLists(element) {
-                        /*close all autocomplete lists in the document,
-                        except the one passed as an argument:*/
-                        var x = document.getElementsByClassName(classNames.autofillItems);
-                        for (var i = 0; i < x.length; i++) {
-                            if (element !== x[i] && element !== autofillInput) {
-                                x[i].parentNode.removeChild(x[i]);
+                        //close all autofill lists in the document, except the one passed as an argument
+                        let self = document.getElementsByClassName(classNames.autofillItems);
+                        for (let i = 0; i < self.length; i++) {
+                            if (element !== self[i] && element !== autofillInput) {
+                                self[i].parentNode.removeChild(self[i]);
                             }
                         }
                     }
 
-                    /*execute a function when someone clicks in the document:*/
-                    document.addEventListener("click", function (e) {
-                        closeAllLists(e.target);
+                    document.addEventListener("click", function (event) {
+                        closeAllLists(event.target);
                     });
                 }
 
                 function emphasizeValueInOption(option, value) {
+                    //highlights the matching phrase 'value' within a suggested 'option'
                     let emphasizedHtml = "";
-
                     let matchStart = 0;
                     let beforeMatch = null;
                     let afterMatch = null;
@@ -152,7 +189,8 @@
                 }
 
                 //requests the available autofill-data and associates it with corresponding input
-                function getAutofillData(url, attributeKeys, autofillInput) {
+                function getAutofillData(endpoint, attributeKeys, autofillInput) {
+                    let url = endpoint;
                     let attributeCount = 1;
                     let requestParams = '?';
 
@@ -163,36 +201,60 @@
                         attributeCount++;
                     });
 
-                    let xhr = new XMLHttpRequest();
-                    xhr.open("GET", url + requestParams, true);
-                    xhr.onload = function (e) {
-                        if (xhr.readyState === 4) {
-                            if (xhr.status === 200) {
-                                let jsonData = JSON.parse(xhr.responseText);
-                                //associate autofill-data with corresponding input field
-                                autofill(autofillInput, Object.keys(jsonData).map((key) => jsonData[key]));
-                            } else {
-                                //don't care
-                                console.error(xhr.statusText);
-                            }
+                    url = url + requestParams;
+
+                    //check if a similar request has already been made
+                    if (cache[url]) {
+                        autofill(autofillInput, cache[url]);
+                    } else if (processing[url] === true) {
+                        if(!requestAttempts[url]) {
+                            requestAttempts[url] = defaults.maxRequestAttempts;
                         }
-                    };
-                    xhr.send(null);
+                        if(requestAttempts[url]-- > 0) {
+                            setTimeout(function () {
+                                getAutofillData(endpoint, attributeKeys, autofillInput);
+                            }, defaults.requestWaitingTime);
+                        }
+                    } else {
+                        processing[url] = true;
+                        let xhr = new XMLHttpRequest();
+                        xhr.open("GET", url, true);
+                        xhr.onload = function (event) {
+                            if (xhr.readyState === 4) {
+                                if (xhr.status === 200) {
+                                    let jsonData = JSON.parse(xhr.responseText);
+                                    let autofillData = Object.keys(jsonData).map((key) => jsonData[key]);
+                                    //associate autofill-data with corresponding input field
+                                    autofill(autofillInput, autofillData);
+                                    processing[url] = false;
+                                    cache[url] = autofillData;
+                                } else {
+                                    //don't care
+                                    console.error(xhr.statusText);
+                                }
+                            }
+                        };
+                        xhr.send(null);
+                    }
+                }
+
+                function assignRandomId(element) {
+                    //Generates a random base64 id and assigns it to thae calling element
+                    let id = btoa(Math.floor(Math.random() * defaults.maxIds).toString());
+                    while (!autofillIds.includes(id)) {
+                        id = btoa(Math.floor(Math.random() * defaults.maxIds).toString());
+                        if (!autofillIds.includes(id)) {
+                            element.setAttribute("id", id);
+                            autofillIds.push(id);
+                        }
+                    }
                 }
 
                 document.querySelectorAll(self.selector).forEach(function (autofillInput) {
                     autofillInput.classList.add(classNames.autofill);
-
-                    if(autofillInput.getAttribute('id') === null) {
-                        let id = btoa(Math.floor(Math.random() * 10000).toString());
-                        console.log(id);
-                        while(!autofillIds.includes(id)) {
-                            id = btoa(Math.floor(Math.random() * 10000).toString());
-                            if (!autofillIds.includes(id)) {
-                                autofillInput.setAttribute("id", id);
-                                autofillIds.push(id);
-                            }
-                        }
+                    //id might be needed later
+                    if (autofillInput.getAttribute('id') === null) {
+                        assignRandomId(autofillInput);
                     }
                     let attributeKeys = JSON.parse(autofillInput.dataset.autofillAttributeKeys);
                     getAutofillData(autofillInput.dataset.autofillEndpoint, attributeKeys, autofillInput);
