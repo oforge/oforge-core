d1858efe9b0816dcfa8e1ad29950cc99570586f3
FK <fk@7pkonzepte.de>
2019-09-06 13:28:05 +0200

    quill

--- Themes/Base/Backend/__assets/js/quill/module/whitespace-button.js
+++ Themes/Base/Backend/__assets/js/quill/module/whitespace-button.js
@@ -20,7 +20,7 @@
         }
     }
 
-    Quill.register('modules/BindFormField', WhitespaceButton);
+    Quill.register('modules/WhitespaceButton', WhitespaceButton);
     let icons = Quill.import('ui/icons');
     icons['WhitespaceButton'] = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M3 4a1 1 0 1 0 0 2h1v18H3a1 1 0 1 0 0 2h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H3zm3 11c0 .256.097.512.293.707l3 3A1 1 0 0 0 11 18v-2h8v2a.999.999 0 0 0 1.707.707l3-3a.996.996 0 0 0 0-1.414l-3-3A1 1 0 0 0 19 12v2h-8v-2a.999.999 0 0 0-1.707-.707l-3 3A.996.996 0 0 0 6 15zm18 0v10a1 1 0 0 0 1 1h2a1 1 0 1 0 0-2h-1V6h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v10z"/></svg>';
 })(jQuery, Quill);
