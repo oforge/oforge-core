2f684e73d09e75e5089ab99737e686c69c95c91b
Der-Alex <alexander.thimm@googlemail.com>
2019-09-06 13:30:36 +0200

    check image mime type and image content

--- Themes/Base/Frontend/__assets/js/image-upload.js
+++ Themes/Base/Frontend/__assets/js/image-upload.js
@@ -9,6 +9,12 @@ if (typeof Oforge !== 'undefined') {
         init: function () {
 
             var self = this;
+            var imageTypes = [
+                'image/jpg',
+                'image/jpeg',
+                'image/gif',
+                'image/png',
+            ];
             checkPlaceholderItem();
 
             function createFileInput(uploadImageList) {
@@ -23,10 +29,31 @@ if (typeof Oforge !== 'undefined') {
                 input.setAttribute('style', 'display: none;');
 
                 input.onchange = function (evt) {
+                    var i = null,
+                        file = null,
+                        image = null;
+
+                    image = new Image();
+
+                    function setError() {
+                        this.removeEventListener('error', setError);
+                        console.warn('error, no image');
+                        deleteListItem(uploadId);
+                        checkPlaceholderItem();
+                        return false;
+                    }
+
                     if (input.files != null) {
-                        for (var i = 0; i < input.files.length; i++) {
-                            var file = input.files[i];
-                            console.log(file);
+                        for (i = 0; i < input.files.length; i++) {
+                            file = input.files[i];
+
+                            if (imageTypes.indexOf(file.type) < 0) {
+                                console.warn('no image');
+                                return false;
+                            }
+
+                            image.addEventListener('error', setError);
+                            image.src = window.URL.createObjectURL(file);
 
                             uploadItemElement = createImageListItem(
                                 uploadId,
@@ -34,9 +61,7 @@ if (typeof Oforge !== 'undefined') {
                                 uploadImageList,
                                 input.files[i].size
                             );
-
                         }
-
                         uploadItemElement.appendChild(input);
                     }
                     checkPlaceholderItem();
