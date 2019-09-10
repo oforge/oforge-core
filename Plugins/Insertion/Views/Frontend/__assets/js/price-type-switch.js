2aaf477a1aac142f3cb9e1d90388a4f0b6b9f7cb
loubis <steffenosterburg@googlemail.com>
2019-09-10 14:21:02 +0200

    added multiple price types

--- Plugins/Insertion/Views/Frontend/__assets/js/price-type-switch.js
+++ Plugins/Insertion/Views/Frontend/__assets/js/price-type-switch.js
@@ -1,34 +1,45 @@
-(function () {
-    if (typeof Oforge !== 'undefined') {
-        var selectionElement = document.getElementById('price-type');
-        selectionElement.addEventListener('change', function () {
-            var minField = document.getElementById('price-min');
+if (typeof Oforge !== 'undefined') {
+    Oforge.register({
+        name: 'priceTypeSwitch',
+        selector: '#price_type',
+        init: function () {
+            var self = this;
+            var selectionElement = document.querySelector(self.selector);
+            var minField = document.getElementById('price_min');
             var maxField = document.getElementById('price');
 
-            if (selectionElement.value === 'on_demand') {
-                minField.required = true;
-                minField.setAttribute('name', 'price-min');
-                maxField.placeholder = getSelectedOption(selectionElement).getAttribute('data-placeholder');
-                minField.closest('.form__control').classList.remove('is-hidden');
-            } else {
-                minField.required = false;
-                minField.removeAttribute('name');
-                maxField.placeholder = getSelectedOption(selectionElement).getAttribute('data-placeholder');
-                minField.closest('.form__control').classList.add('is-hidden');
-            }
-        });
-    } else {
-        console.warn("Oforge is not defined. Module cannot be registered.");
-    }
-})();
+            changeHandler();
 
-function getSelectedOption(selection) {
-    var candidate;
-    for (var i = 0, len = selection.options.length; i < len; i++) {
-        candidate = selection.options[i];
-        if (candidate.selected === true) {
-            break;
+            selectionElement.addEventListener('change', function () {
+                changeHandler();
+            });
+
+            function changeHandler() {
+                if (selectionElement.value === 'on_demand') {
+                    minField.required = true;
+                    minField.setAttribute('name', 'price_min');
+                    maxField.placeholder = getSelectedOption(selectionElement).getAttribute('data-placeholder');
+                    minField.closest('.form__control').classList.remove('is-hidden');
+                } else {
+                    minField.required = false;
+                    minField.removeAttribute('name');
+                    maxField.placeholder = getSelectedOption(selectionElement).getAttribute('data-placeholder');
+                    minField.closest('.form__control').classList.add('is-hidden');
+                }
+            }
+
+            function getSelectedOption(selection) {
+                var candidate;
+                for (var i = 0, len = selection.options.length; i < len; i++) {
+                    candidate = selection.options[i];
+                    if (candidate.selected === true) {
+                        break;
+                    }
+                }
+                return candidate;
+            }
         }
-    }
-    return candidate;
+    });
+} else {
+    console.warn("Oforge is not defined. Module cannot be registered.");
 }
