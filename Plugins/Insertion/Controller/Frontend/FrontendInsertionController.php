e86fdd9d192c0445a79eb05a4afa05b62df85702
kevinkoessl <kevin.koessl@gmail.com>
2019-09-19 17:30:51 +0200

    Ticket 41 autofill address. Fixed a problem where an exception was thrown if you create an insertion without being logged in.

--- Plugins/Insertion/Controller/Frontend/FrontendInsertionController.php
+++ Plugins/Insertion/Controller/Frontend/FrontendInsertionController.php
@@ -131,11 +131,14 @@ class FrontendInsertionController extends SecureFrontendController
                 /** @var InsertionService $insertionService */
                 $insertionService = Oforge()->Services()->get('insertion');
                 $user = $userService->getUser();
-                /** @var Insertion $lastInsertion */
-                $lastInsertion = $insertionService->getLatestInsertionOfUser($user->getId());
 
-                if(isset($lastInsertion)) {
-                    $result['lastContact'] = $lastInsertion->getContact()->toArray(1);
+                if(isset($user)) {
+                    /** @var Insertion $lastInsertion */
+                    $lastInsertion = $insertionService->getLatestInsertionOfUser($user->getId());
+
+                    if (isset($lastInsertion)) {
+                        $result['lastContact'] = $lastInsertion->getContact()->toArray(1);
+                    }
                 }
             }
         }
