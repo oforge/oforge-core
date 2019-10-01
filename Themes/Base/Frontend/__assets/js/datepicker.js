8bf38bd78663d2077efd182e1cca1db2aef888f3
Basti <sk@7pkonzepte.de>
2019-10-01 21:16:46 +0200

    included pickaday datepicker

---
+++ Themes/Base/Frontend/__assets/js/datepicker.js
@@ -0,0 +1,17 @@
+if (typeof Oforge !== 'undefined') {
+    Oforge.register({
+        name: 'datepicker',
+        selector: '#datepicker',
+        otherNotRequiredContent: 'some other content, that we can define and that is not required',
+        init: function () {
+            var picker = new Pikaday({
+                field: document.getElementById('datepicker'),
+                maxDate: new Date(),
+                yearRange: 40,
+            });
+            console.log(picker);
+        }
+    });
+} else {
+    console.warn("Oforge is not defined. Module cannot be registered.");
+}
