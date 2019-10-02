9521d3918aa6d7f671e543aa9026cf5df43d785d
Basti <sk@7pkonzepte.de>
2019-10-02 13:42:00 +0200

    added german localisation to datepicker

--- Themes/Base/Frontend/__assets/js/datepicker.js
+++ Themes/Base/Frontend/__assets/js/datepicker.js
@@ -4,12 +4,28 @@ if (typeof Oforge !== 'undefined') {
         selector: '#datepicker',
         otherNotRequiredContent: 'some other content, that we can define and that is not required',
         init: function () {
+            var me = this;
+            var lang = document.documentElement.lang;
+            var localisation = {
+                de: {
+                    previousMonth : 'Voriger Monat',
+                    nextMonth     : 'Nächster Monat',
+                    months        : ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
+                    weekdays      : ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+                    weekdaysShort : ['So','Mo','Di','Mi','Do','Fr','Sa']
+                },
+            };
             var picker = new Pikaday({
                 field: document.getElementById('datepicker'),
                 maxDate: moment().toDate(),
                 minDate: moment().subtract(40, 'years').toDate(),
+                yearRange: 40,
+
             });
-            console.log(picker);
+
+            if(lang in localisation) {
+                picker._o.i18n = localisation[lang];
+            }
         }
     });
 } else {
