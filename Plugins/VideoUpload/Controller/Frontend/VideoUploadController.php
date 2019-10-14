d7c42773b2b60be8eb3dc15da6caddac2eb9b6d3
kevinkoessl <kevin.koessl@gmail.com>
2019-10-14 09:51:18 +0200

    Function for requesting a Vimeo Video Key for a given Insertion Id.

--- Plugins/VideoUpload/Controller/Frontend/VideoUploadController.php
+++ Plugins/VideoUpload/Controller/Frontend/VideoUploadController.php
@@ -10,6 +10,8 @@ use Oforge\Engine\Modules\Core\Services\ConfigService;
 use Oforge\Engine\Modules\I18n\Helper\I18N;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use VideoUpload\Models\VideoKey;
+use VideoUpload\Services\VideoUploadService;
 use Vimeo\Exceptions\VimeoRequestException;
 use Vimeo\Vimeo;
 
@@ -27,36 +29,6 @@ class VideoUploadController
     public function indexAction(Request $request, Response $response) {
     }
 
-    /**
-     * @param Request $request
-     * @param Response $response
-     *
-     * @return void
-     * @throws ServiceNotFoundException
-     * @throws VimeoRequestException
-     * @throws \Doctrine\ORM\ORMException
-     * @EndpointAction()
-     */
-    public function testAction(Request $request, Response $response) {
-        /** @var ConfigService $configService */
-        $configService = Oforge()->Services()->get('config');
-
-        /** @var Config[] $groupConfigs */
-        $groupConfigs = $configService->getGroupConfigs('vimeo');
-        $vimeoGroupConfigs = [];
-        foreach ($groupConfigs as $config) {
-            $vimeoGroupConfigs[$config->getName()] = $config->getValues()[0]->getValue();
-        }
-        if (isset($vimeoGroupConfigs) && !empty($vimeoGroupConfigs)) {
-            $client = new Vimeo($vimeoGroupConfigs['vimeo_client_id'], $vimeoGroupConfigs['vimeo_client_secret'], $vimeoGroupConfigs['vimeo_access_token']);
-
-            //$apiResponse = $client->upload(ROOT_PATH . '/var/public/videos/Horses_3.mp4');
-
-            //$apiResponse = $client->request('/tutorial', array(), 'GET');
-            //Oforge()->View()->assign(['json' => $apiResponse]);
-        }
-    }
-
     public function credentialsAction(Request $request, Response $response) {
         $configService = Oforge()->Services()->get('config');
         /** @var Config[] $groupConfigs */
@@ -67,5 +39,26 @@ class VideoUploadController
         if (isset($vimeoGroupConfigs) && !empty($vimeoGroupConfigs)) {
             Oforge()->View()->assign(['json' => $vimeoGroupConfigs]);
         }
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @throws ServiceNotFoundException
+     * @EndpointAction(path="/id/{id}")
+     */
+    public function getVideoKeyAction(Request $request, Response $response, $args){
+        $insertionId = $args['id'];
+        $jsonResponse = [];
+
+        /** @var VideoUploadService $videoUploadService */
+        $videoUploadService = Oforge()->Services()->get('video.upload');
+
+        /** @var VideoKey $videoKey */
+        $videoKey = $videoUploadService->getVideoKey($insertionId);
+        $jsonResponse['vimeo_video_key'] = $videoKey->getVideoKey();
+
+        Oforge()->View()->assign(['json' => $jsonResponse]);
     }
 }
