ef03ed5892daaea2c07623a47bd6193199b150c1
Der-Alex <alexander.thimm@googlemail.com>
2019-10-25 14:02:08 +0200

    extended modal view with single auto-open

--- Themes/Base/Frontend/__assets/js/modal-view.js
+++ Themes/Base/Frontend/__assets/js/modal-view.js
@@ -8,6 +8,10 @@
             },
             init: function () {
                 var self = this;
+                var modal = document.querySelector(self.selector);
+                if (modal.hasAttribute('data-auto-open') && !getAutoModalSeen(modal.dataset.modalView)) {
+                    openModalView(modal.dataset.modalView);
+                }
 
                 function openModalView(id) {
                     var modalViewContent = document.querySelector('[data-modal-content="' + id + '"]');
@@ -15,8 +19,20 @@
                 }
 
                 function closeModalView(id) {
+                    var modal = document.querySelector('[data-modal-view="' + id + '"]');
                     var modalViewContent = document.querySelector('[data-modal-content="' + id + '"]');
                     modalViewContent.classList.remove('is--visible');
+                    if (modal.hasAttribute('data-auto-open')) {
+                        setAutoModalSeen(id);
+                    }
+                }
+
+                function setAutoModalSeen(id) {
+                    sessionStorage.setItem('oforge_' + id, '1');
+                }
+
+                function getAutoModalSeen(id) {
+                    return sessionStorage.getItem('oforge_' + id);
                 }
 
                 document.addEventListener('click', function (evt) {
