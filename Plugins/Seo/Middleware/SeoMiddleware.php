f7aaba476c3b95fe6c5ea0548ae3dbe83c29960e
Der-Alex <alexander.thimm@googlemail.com>
2019-11-05 10:41:28 +0100

    added comments to seo middleware

--- Plugins/Seo/Middleware/SeoMiddleware.php
+++ Plugins/Seo/Middleware/SeoMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace Seo\Middleware;
 
+use Oforge\Engine\Modules\Core\Exceptions\ServiceNotFoundException;
 use Oforge\Engine\Modules\Core\Helper\RouteHelper;
 use Seo\Models\SeoUrl;
 use Seo\Services\SeoService;
@@ -9,6 +10,19 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 class SeoMiddleware {
+    /**
+     * When a SEO url is requested:
+     * - check the query params
+     * - find the matching endpoint
+     * - respond with result from that endpoint
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $next
+     *
+     * @return mixed
+     * @throws ServiceNotFoundException
+     */
     public function __invoke(Request $request,Response $response, $next) {
         $uri  = $request->getUri();
         $params = $request->getQueryParams();
@@ -28,7 +42,12 @@ class SeoMiddleware {
             $newUri = $uri->withPath($queryParams['url']);
             $request = $request->withUri($newUri);
             if (!empty($params)) {
-                //$request = $request->withQueryParams($params);
+                /**
+                 * The request still has the query params. We only need to change the path to the original endpoint
+                 * That happens in these lines:
+                 * $newUri = $uri->withPath($queryParams['url']);
+                 * $request = $request->withUri($newUri);
+                 */
             } elseif(!empty($queryParams['query_params'])) {
                 $request = $request->withQueryParams($queryParams['query_params']);
             }
