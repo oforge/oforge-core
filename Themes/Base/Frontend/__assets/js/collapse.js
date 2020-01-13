e2db9ea861cddc8e1d271d94d62adbec06fb2793
Der-Alex <alexander.thimm@googlemail.com>
2020-01-13 10:55:42 +0100

    premium price selection and detail collapsible

--- Themes/Base/Frontend/__assets/js/collapse.js
+++ Themes/Base/Frontend/__assets/js/collapse.js
@@ -1,11 +1,13 @@
 /**
  * Adds a simple collapse functionality.
  *
- * Usage: Collapse triggers will have the 'data-collapse' attribute. ( <element data-collapse="" > )
- * The next element on the same level as the trigger will be toggled with the class "collapsed".
+ * Usage: Collapse triggers will have the 'data-collapse' attribute. ( <element data-collapse='' > )
+ * The next element on the same level as the trigger will be toggled with the class 'collapsed'.
  *
  * Optional: if 'data-collapse' is set to a value, triggers will only be applied if screen-width < value.
  *
+ * Extension: If the collapse trigger element has an attribute data-collapse-target and that target contains a css class
+ * then all elements with that class will be triggered
  */
 
 (function () {
@@ -21,14 +23,24 @@
                     let triggerWidth = parseInt(trigger.dataset.collapse);
                     if (Number.isNaN(triggerWidth) || clientWidth < triggerWidth ) {
                         trigger.addEventListener('click', function () {
-                            this.classList.toggle("active");
-                            this.nextElementSibling.classList.toggle("collapsed");
+                            if (this.hasAttribute('data-collapse-target')) {
+                                triggers.forEach(function (trigger) {
+                                    trigger.classList.toggle('active');
+                                });
+                                let targets = document.querySelectorAll(trigger.dataset.collapseTarget);
+                                targets.forEach(function (target) {
+                                    target.classList.toggle('collapsed');
+                                });
+                            } else {
+                                this.classList.toggle('active');
+                                this.nextElementSibling.classList.toggle('collapsed');
+                            }
                         });
                     }
                 });
             }
         });
     } else {
-        console.warn("Oforge is not defined. Module cannot be registered.");
+        console.warn('Oforge is not defined. Module cannot be registered.');
     }
 })();
