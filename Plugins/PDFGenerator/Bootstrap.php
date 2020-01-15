1b20760031f1497435d63ee909d130b85844897c
Der-Alex <alexander.thimm@googlemail.com>
2020-01-15 13:17:02 +0100

    added pdf config

--- Plugins/PDFGenerator/Bootstrap.php
+++ Plugins/PDFGenerator/Bootstrap.php
@@ -3,7 +3,6 @@
 namespace PDFGenerator;
 
 use Oforge\Engine\Modules\Core\Abstracts\AbstractBootstrap;
-use PDFGenerator\Controller\PDFTestController;
 use PDFGenerator\Services\PDFGeneratorService;
 
 class Bootstrap extends AbstractBootstrap {
