1eb175f54365f0817f7c848cb0b9d20592caaef1
FK <fk@7pkonzepte.de>
2020-06-25 12:05:59 +0200

    Extend linkPlugin node script

--- devtools/linkPlugin.js
+++ devtools/linkPlugin.js
@@ -1,15 +1,21 @@
 if (process.argv.length < 3) {
     console.error('Require path to source folder!');
-    console.log("Run 'npm run linkPlugin <src>'.");
+    console.log("Run 'npm run linkPlugin <src> [<PluginFolderName>]'.");
 } else {
     const lnk = require('lnk');
     const fs = require('fs');
     const path = require('path');
     const dst = './Plugins';
     const srcPath = process.argv[2];
+    console.log(process.argv);
     if (fs.existsSync(dst)) {
-        lnk(srcPath, dst)
-            .then(() => console.log('Created link to plugin folder: ' + path.basename(srcPath)));
+        if (process.argv.length === 3) {
+            lnk(srcPath, dst)
+                .then(() => console.log('Created link to plugin folder: ' + path.basename(srcPath)));
+        } else {
+            lnk(srcPath, dst, {rename: process.argv[3]})
+                .then(() => console.log('Created link to plugin folder: ' + path.basename(srcPath)));
+        }
     } else {
         console.error('Could not access plugin folder!');
     }
